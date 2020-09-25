92881844d7fecb06e8c05a36d62d54e5c887d18a
nmadruga <katinachovansky@gmail.com>
2020-09-25 13:07:01 +1200

    Fix build

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,40 +1,34 @@
 const commonPaths = require('./common-paths')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const port = process.env.PORT || 3000
 
 const config = {
   mode: 'development',
   output: {
-    filename: '[name].[hash].js'
+    filename: '[name].[hash].js',
   },
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-                importLoaders: 2,
-            },
-          },
-          'postcss-loader',
-          'less-loader'
-        ]
-      }
-    ]
+        test: /\.(less)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+    ],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: './css/[name].css',
+    }),
   ],
   devServer: {
     host: 'localhost',
     port: port,
     historyApiFallback: true,
     hot: true,
-    open: true
-  }
-};
+    open: true,
+  },
+}
 module.exports = config
