de74a437a1729664a0f3f2bb135b79ad82c00fd4
nmadruga <katinachovansky@gmail.com>
2020-10-22 14:51:49 +1300

    Fix compilation

--- src/elementPlugins/textInfo/src/ApplicationView.tsx
+++ src/elementPlugins/textInfo/src/ApplicationView.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Message } from 'semantic-ui-react'
 import { ApplicationViewProps } from '../../types'
 
-const ApplicationView: React.FC<ApplicatioViewProps> = ({ templateElement: { parameters } }) => {
+const ApplicationView: React.FC<ApplicationViewProps> = ({ templateElement: { parameters } }) => {
   return (
     <Message>
       <Message.Header>{parameters.title}</Message.Header>
