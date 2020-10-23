9a27453069e429705927bfc5dc69cbab89f4707b
nmadruga <katinachovansky@gmail.com>
2020-10-23 14:03:07 +1300

    Create cache in separated file - test of directive

---
+++ src/cache.ts
@@ -0,0 +1,20 @@
+import { InMemoryCache } from '@apollo/client'
+import { TemplateElementsConnection } from './utils/generated/graphql'
+
+const cache = new InMemoryCache({
+    typePolicies: {
+      TemplateSection: {
+        fields: {
+          pageCount: {
+              read(_, { readField }) {
+                // TODO: Move code to find number of pages per section (in useLoadTemplete) to here
+                  const elementsConnection = readField('templateElementsBySectionId') as TemplateElementsConnection
+                  return 0
+              }
+          }
+        }
+      }
+    }
+})
+
+export default cache
