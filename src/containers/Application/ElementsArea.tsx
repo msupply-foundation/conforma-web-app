4d9bc54ee1758e163d8bd85447d27812ddbebc58
nmadruga <katinachovansky@gmail.com>
2020-11-02 09:45:34 +1300

    Remove next and previous buttons from ElementsArea

--- src/containers/Application/ElementsArea.tsx
+++ src/containers/Application/ElementsArea.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container, Grid, Header, Label, Segment } from 'semantic-ui-react'
+import { Header, Label, Segment } from 'semantic-ui-react'
 import { ApplicationViewWrapper } from '../../elementPlugins'
 import useLoadElements from '../../utils/hooks/useLoadElements'
 import { Loading } from '../../components'
@@ -10,10 +10,6 @@ interface ElementsAreaProps {
   sectionTitle: string
   sectionTemplateId: number
   sectionPage: number
-  isFirstPage: boolean
-  isLastPage: boolean
-  onNextClicked: () => void
-  onPreviousClicked: () => void
 }
 
 const ElementsArea: React.FC<ElementsAreaProps> = ({
@@ -21,10 +17,6 @@ const ElementsArea: React.FC<ElementsAreaProps> = ({
   sectionTitle,
   sectionTemplateId: sectionTempId,
   sectionPage,
-  isFirstPage,
-  isLastPage,
-  onNextClicked,
-  onPreviousClicked,
 }) => {
   const { elements, loading, error } = useLoadElements({
     applicationId,
@@ -60,47 +52,9 @@ const ElementsArea: React.FC<ElementsAreaProps> = ({
           isEditable={true}
         />
       ))}
-      <Grid columns={3}>
-        <Grid.Row>
-          <Grid.Column>
-            {!isFirstPage && (
-              <PageButton title="Previous" type="left" onClicked={onPreviousClicked} />
-            )}
-          </Grid.Column>
-          <Grid.Column />
-          {/* Empty cell */}
-          <Grid.Column>
-            {!isLastPage ? (
-              <PageButton title="Next" type="right" onClicked={onNextClicked} />
-            ) : (
-              <PageButton title="Summary" type="right" onClicked={onNextClicked} />
-            )}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
     </Segment>
   ) : (
     <Label content="Elements area can't be displayed" />
-  )
-}
-
-type ButtonDirection = 'left' | 'right'
-
-interface PageButtonProps {
-  title: string
-  type: ButtonDirection
-  onClicked: () => void
-}
-
-const PageButton: React.FC<PageButtonProps> = (props) => {
-  const { title, type, onClicked } = props
-  return (
-    <Button
-      labelPosition={type}
-      icon={type === 'right' ? 'right arrow' : 'left arrow'}
-      content={title}
-      onClick={() => onClicked()}
-    />
   )
 }
 
