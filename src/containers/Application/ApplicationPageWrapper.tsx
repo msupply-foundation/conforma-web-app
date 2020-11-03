1b96c961b62ffe19da337bc271a881e0f3ea4709
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-11-04 12:48:05 +1300

    Bug fix evaluate expression

--- src/containers/Application/ApplicationPageWrapper.tsx
+++ src/containers/Application/ApplicationPageWrapper.tsx
@@ -61,44 +61,44 @@ const ApplicationPage: React.FC = () => {
       promiseArray.push(evaluateSingleElement(element))
     })
 
-    Promise.all(promiseArray).then((evaluatedElements) => {
-      console.log('Evaluated Elements', evaluatedElements)
-      const elementsState: any = {}
-      evaluatedElements.forEach((element) => {
-        elementsState[element.code] = {
-          id: element.id,
-          category: element.category,
-          isEditable: element.isEditable,
-          isRequired: element.isRequired,
-          isVisible: element.isVisible,
-          // isValid: element.isValid,
-        }
-      })
-      console.log('Elements state', elementsState)
-      return elementsState
+    const evaluatedElements = await Promise.all(promiseArray)
+
+    const elementsState: any = {}
+    evaluatedElements.forEach((element) => {
+      elementsState[element.code] = {
+        id: element.id,
+        category: element.category,
+        isEditable: element.isEditable,
+        isRequired: element.isRequired,
+        isVisible: element.isVisible,
+        // isValid: element.isValid,
+      }
     })
+    return elementsState
   }
 
   async function evaluateSingleElement(element: any) {
+    const evaluationParameters = {
+      objects: [responsesByCode], // TO-DO: Also send user/org objects etc.
+      // graphQLConnection: TO-DO
+    }
     const isEditable = evaluateExpression(element.isEditable)
     const isRequired = evaluateExpression(element.isRequired)
     const isVisible = evaluateExpression(element.visibilityCondition)
     // const isValid = evaluateExpression(element.validation)
 
-    Promise.all([isEditable, isRequired, isVisible]).then((results) => {
-      const evaluatedElement = {
-        code: element.code,
-        id: element.id,
-        category: element.category,
-        isEditable: results[0],
-        isRequired: results[1],
-        isVisible: results[2],
-        // isValid: results[3],
-      }
-      console.log('Single Element', evaluatedElement)
-      return evaluatedElement
-    })
-    return {}
+    const results = await Promise.all([isEditable, isRequired, isVisible])
+
+    const evaluatedElement = {
+      code: element.code,
+      id: element.id,
+      category: element.category,
+      isEditable: results[0],
+      isRequired: results[1],
+      isVisible: results[2],
+      // isValid: results[3],
+    }
+    return evaluatedElement
   }
 
   return error ? (
