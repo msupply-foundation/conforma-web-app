6bab6602bc4b0ef44e5d9b60450f6194f9ca9030
nmadruga <nmadruga@users.noreply.github.com>
2020-11-10 14:51:52 +1300

    Improve comment
    Co-authored-by: Carl Smith <5456533+CarlosNZ@users.noreply.github.com>

--- src/utils/hooks/useListApplications.tsx
+++ src/utils/hooks/useListApplications.tsx
@@ -17,7 +17,7 @@ const useListApplication = () => {
   useEffect(() => {
     if (apolloError) return
     if (apolloLoading) return
-    // Check that only one tempalte matched
+   // Check that only one template matched
     let error = checkForApplicationsErrors(data)
     if (error) {
       setError(error)
