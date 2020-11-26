c51ebad05a91a91dadd361a977c947b1b7c7fb7f
nmadruga <katinachovansky@gmail.com>
2020-11-26 22:59:08 +1300

    Fix compilation

--- src/utils/generated/graphql.tsx
+++ src/utils/generated/graphql.tsx
@@ -16887,12 +16887,12 @@ export type ApplicationFragment = (
 
 export type ElementFragment = (
   { __typename?: 'TemplateElement' }
-  & Pick<TemplateElement, 'id' | 'title' | 'elementTypePluginCode' | 'code' | 'category' | 'isEditable' | 'isRequired' | 'visibilityCondition'>
+  & Pick<TemplateElement, 'id' | 'title' | 'elementTypePluginCode' | 'code' | 'category' | 'isEditable' | 'isRequired' | 'parameters' | 'visibilityCondition'>
 );
 
 export type ResponseFragment = (
   { __typename?: 'ApplicationResponse' }
-  & Pick<ApplicationResponse, 'id' | 'value' | 'timeCreated' | 'isValid'>
+  & Pick<ApplicationResponse, 'id' | 'value' | 'isValid' | 'timeCreated'>
 );
 
 export type SectionFragment = (
@@ -16911,6 +16911,8 @@ export type CreateApplicationMutationVariables = Exact<{
   templateId: Scalars['Int'];
   outcome?: Maybe<ApplicationOutcome>;
   trigger?: Maybe<Trigger>;
+  sections?: Maybe<Array<ApplicationSectionApplicationIdFkeyApplicationSectionCreateInput>>;
+  responses?: Maybe<Array<ApplicationResponseApplicationIdFkeyApplicationResponseCreateInput>>;
 }>;
 
 
@@ -16932,62 +16934,6 @@ export type CreateApplicationMutation = (
         & TemplateFragment
       )> }
       & ApplicationFragment
-    )> }
-  )> }
-);
-
-export type CreateResponseMutationVariables = Exact<{
-  applicationId: Scalars['Int'];
-  templateElementId: Scalars['Int'];
-  timeCreated: Scalars['Datetime'];
-}>;
-
-
-export type CreateResponseMutation = (
-  { __typename?: 'Mutation' }
-  & { createApplicationResponse?: Maybe<(
-    { __typename?: 'CreateApplicationResponsePayload' }
-    & { applicationResponse?: Maybe<(
-      { __typename?: 'ApplicationResponse' }
-      & { templateElement?: Maybe<(
-        { __typename?: 'TemplateElement' }
-        & ElementFragment
-      )>, application?: Maybe<(
-        { __typename?: 'Application' }
-        & Pick<Application, 'serial'>
-      )> }
-      & ResponseFragment
-    )> }
-  )> }
-);
-
-export type CreateSectionMutationVariables = Exact<{
-  applicationId: Scalars['Int'];
-  templateSectionId: Scalars['Int'];
-}>;
-
-
-export type CreateSectionMutation = (
-  { __typename?: 'Mutation' }
-  & { createApplicationSection?: Maybe<(
-    { __typename?: 'CreateApplicationSectionPayload' }
-    & { applicationSection?: Maybe<(
-      { __typename?: 'ApplicationSection' }
-      & Pick<ApplicationSection, 'id'>
-      & { templateSection?: Maybe<(
-        { __typename?: 'TemplateSection' }
-        & { templateElementsBySectionId: (
-          { __typename?: 'TemplateElementsConnection' }
-          & { nodes: Array<Maybe<(
-            { __typename?: 'TemplateElement' }
-            & ElementFragment
-          )>> }
-        ) }
-        & SectionFragment
-      )>, application?: Maybe<(
-        { __typename?: 'Application' }
-        & Pick<Application, 'id' | 'serial'>
-      )> }
     )> }
   )> }
 );
@@ -17144,6 +17090,7 @@ export type GetElementsAndResponsesQuery = (
         )>> }
       ) }
     )> }
+    & ApplicationFragment
   )> }
 );
 
@@ -17270,6 +17217,7 @@ export const ElementFragmentDoc = gql`
   category
   isEditable
   isRequired
+  parameters
   visibilityCondition
 }
     `;
@@ -17277,6 +17225,7 @@ export const ResponseFragmentDoc = gql`
     fragment Response on ApplicationResponse {
   id
   value
+  isValid
   timeCreated
   isValid
 }
@@ -17298,8 +17247,8 @@ export const TemplateFragmentDoc = gql`
 }
     `;
 export const CreateApplicationDocument = gql`
-    mutation createApplication($name: String!, $serial: String!, $templateId: Int!, $outcome: ApplicationOutcome = PENDING, $trigger: Trigger = ON_APPLICATION_CREATE) {
-  createApplication(input: {application: {name: $name, serial: $serial, templateId: $templateId, isActive: true, outcome: $outcome, trigger: $trigger}}) {
+    mutation createApplication($name: String!, $serial: String!, $templateId: Int!, $outcome: ApplicationOutcome = PENDING, $trigger: Trigger = ON_APPLICATION_CREATE, $sections: [ApplicationSectionApplicationIdFkeyApplicationSectionCreateInput!], $responses: [ApplicationResponseApplicationIdFkeyApplicationResponseCreateInput!]) {
+  createApplication(input: {application: {name: $name, serial: $serial, templateId: $templateId, isActive: true, outcome: $outcome, trigger: $trigger, applicationSectionsUsingId: {create: $sections}, applicationResponsesUsingId: {create: $responses}}}) {
     application {
       ...Application
       template {
@@ -17336,6 +17285,8 @@ export type CreateApplicationMutationFn = Apollo.MutationFunction<CreateApplicat
  *      templateId: // value for 'templateId'
  *      outcome: // value for 'outcome'
  *      trigger: // value for 'trigger'
+ *      sections: // value for 'sections'
+ *      responses: // value for 'responses'
  *   },
  * });
  */
@@ -17345,97 +17296,6 @@ export function useCreateApplicationMutation(baseOptions?: Apollo.MutationHookOp
 export type CreateApplicationMutationHookResult = ReturnType<typeof useCreateApplicationMutation>;
 export type CreateApplicationMutationResult = Apollo.MutationResult<CreateApplicationMutation>;
 export type CreateApplicationMutationOptions = Apollo.BaseMutationOptions<CreateApplicationMutation, CreateApplicationMutationVariables>;
-export const CreateResponseDocument = gql`
-    mutation createResponse($applicationId: Int!, $templateElementId: Int!, $timeCreated: Datetime!) {
-  createApplicationResponse(input: {applicationResponse: {applicationId: $applicationId, templateElementId: $templateElementId, timeCreated: $timeCreated}}) {
-    applicationResponse {
-      ...Response
-      templateElement {
-        ...Element
-      }
-      application {
-        serial
-      }
-    }
-  }
-}
-    ${ResponseFragmentDoc}
-${ElementFragmentDoc}`;
-export type CreateResponseMutationFn = Apollo.MutationFunction<CreateResponseMutation, CreateResponseMutationVariables>;
-
-/**
- * __useCreateResponseMutation__
- *
- * To run a mutation, you first call `useCreateResponseMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useCreateResponseMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [createResponseMutation, { data, loading, error }] = useCreateResponseMutation({
- *   variables: {
- *      applicationId: // value for 'applicationId'
- *      templateElementId: // value for 'templateElementId'
- *      timeCreated: // value for 'timeCreated'
- *   },
- * });
- */
-export function useCreateResponseMutation(baseOptions?: Apollo.MutationHookOptions<CreateResponseMutation, CreateResponseMutationVariables>) {
-        return Apollo.useMutation<CreateResponseMutation, CreateResponseMutationVariables>(CreateResponseDocument, baseOptions);
-      }
-export type CreateResponseMutationHookResult = ReturnType<typeof useCreateResponseMutation>;
-export type CreateResponseMutationResult = Apollo.MutationResult<CreateResponseMutation>;
-export type CreateResponseMutationOptions = Apollo.BaseMutationOptions<CreateResponseMutation, CreateResponseMutationVariables>;
-export const CreateSectionDocument = gql`
-    mutation createSection($applicationId: Int!, $templateSectionId: Int!) {
-  createApplicationSection(input: {applicationSection: {applicationId: $applicationId, templateSectionId: $templateSectionId}}) {
-    applicationSection {
-      id
-      templateSection {
-        ...Section
-        templateElementsBySectionId {
-          nodes {
-            ...Element
-          }
-        }
-      }
-      application {
-        id
-        serial
-      }
-    }
-  }
-}
-    ${SectionFragmentDoc}
-${ElementFragmentDoc}`;
-export type CreateSectionMutationFn = Apollo.MutationFunction<CreateSectionMutation, CreateSectionMutationVariables>;
-
-/**
- * __useCreateSectionMutation__
- *
- * To run a mutation, you first call `useCreateSectionMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useCreateSectionMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [createSectionMutation, { data, loading, error }] = useCreateSectionMutation({
- *   variables: {
- *      applicationId: // value for 'applicationId'
- *      templateSectionId: // value for 'templateSectionId'
- *   },
- * });
- */
-export function useCreateSectionMutation(baseOptions?: Apollo.MutationHookOptions<CreateSectionMutation, CreateSectionMutationVariables>) {
-        return Apollo.useMutation<CreateSectionMutation, CreateSectionMutationVariables>(CreateSectionDocument, baseOptions);
-      }
-export type CreateSectionMutationHookResult = ReturnType<typeof useCreateSectionMutation>;
-export type CreateSectionMutationResult = Apollo.MutationResult<CreateSectionMutation>;
-export type CreateSectionMutationOptions = Apollo.BaseMutationOptions<CreateSectionMutation, CreateSectionMutationVariables>;
 export const CreateUserDocument = gql`
     mutation createUser($email: String!, $password: String!, $username: String!) {
   createUser(input: {user: {email: $email, passwordHash: $password, username: $username}}) {
@@ -17648,6 +17508,7 @@ export type GetApplicationsQueryResult = Apollo.QueryResult<GetApplicationsQuery
 export const GetElementsAndResponsesDocument = gql`
     query getElementsAndResponses($serial: String!) {
   applicationBySerial(serial: $serial) {
+    ...Application
     applicationResponses {
       nodes {
         ...Response
@@ -17670,7 +17531,8 @@ export const GetElementsAndResponsesDocument = gql`
     }
   }
 }
-    ${ResponseFragmentDoc}
+    ${ApplicationFragmentDoc}
+${ResponseFragmentDoc}
 ${SectionFragmentDoc}
 ${ElementFragmentDoc}`;
 
