e6df4984b239569c8545664e442d9e5f4473cf70
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-11-27 12:01:47 +1300

    Tidy up

--- src/utils/helpers/loginCheck.ts
+++ src/utils/helpers/loginCheck.ts
@@ -1,8 +1,5 @@
 const isLoggedIn = () => {
-  // const username = localStorage.getItem('username')
-  // const templatePermissions = localStorage.getItem('templatePermissions')
   const JWT = localStorage.getItem('persistJWT')
-  console.log('Logged in?', JWT !== null, JWT)
   return JWT !== null
 }
 
