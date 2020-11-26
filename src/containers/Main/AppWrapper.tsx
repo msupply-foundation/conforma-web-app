6a85b2c82e5fcb32e35fda2092fbdd9c76f8149d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-11-26 15:40:26 +1300

    Routing for not logged in

--- src/containers/Main/AppWrapper.tsx
+++ src/containers/Main/AppWrapper.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
-import { Footer } from '../../components'
+import { Footer, Login } from '../../components'
 import SiteLayout from './SiteLayout'
 import UserArea from '../User/UserArea'
 import { UserProvider } from '../../contexts/UserState'
+import isLoggedIn from '../../utils/helpers/loginCheck'
 
 const AppWrapper: React.FC = () => {
   return (
-    <div>
+    <Router>
       <UserProvider>
-        <UserArea />
-        <SiteLayout />
+        {!isLoggedIn() && <Redirect to="/login" />}
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route>
+            <UserArea />
+            <SiteLayout />
+            <Footer />
+          </Route>
+        </Switch>
       </UserProvider>
-      <Footer />
-    </div>
+    </Router>
   )
 }
 
---
+++ src/utils/helpers/loginCheck.ts
@@ -0,0 +1,5 @@
+const isLoggedIn = () => {
+  return true
+}
+
+export default isLoggedIn
