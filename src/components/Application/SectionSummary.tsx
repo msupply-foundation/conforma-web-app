98152fbead565c387d83ac969958f716412ce301
nmadruga <katinachovansky@gmail.com>
2020-11-30 16:51:00 +1300

    Display summary using new helper to group by section and page

--- src/components/Application/SectionSummary.tsx
+++ src/components/Application/SectionSummary.tsx
@@ -1,52 +1,22 @@
 import React from 'react'
-import { Button, Container, Form, Grid, Header, Segment } from 'semantic-ui-react'
+import { Header, Segment } from 'semantic-ui-react'
 import { SummaryViewWrapper } from '../../formElementPlugins'
-import { SectionElementStates } from '../../utils/types'
+import { ElementAndResponse, SectionElementStates } from '../../utils/types'
 
 interface ApplicationSummaryProps {
-  sectionsAndElements: SectionElementStates[]
-  onSubmitHandler: () => void
-  appStatus: any
+  sectionPages: SectionElementStates
+  editable: boolean
 }
 
-const ApplicationSummary: React.FC<ApplicationSummaryProps> = ({
-  sectionsAndElements,
-  onSubmitHandler,
-  appStatus: { status: status },
-}) => {
+const ApplicationSummary: React.FC<ApplicationSummaryProps> = ({ sectionPages, editable }) => {
+  const { sectionTitle, pages } = sectionPages
   return (
-    <Container style={{ marginTop: '2em' }}>
-      <Header as="h1" content="REVIEW AND SUBMIT" />
-      <Form>
-        {sectionsAndElements.map(({ section, elements }) => (
-          <Segment.Group size="large">
-            <Grid columns={2}>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header as="h2" content={`${section.title}`} />
-                </Grid.Column>
-                <Grid.Column>
-                  {status === 'DRAFT' ? (
-                    <Header as="h5" icon="pencil" floated="right" content="edit" color="blue" />
-                  ) : null}
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            {elements.map((element) => (
-              <SummaryViewWrapper element={element.element} value={element.value} />
-            ))}
-          </Segment.Group>
-        ))}
-        {status === 'DRAFT' ? (
-          <Button
-            content="Submit application"
-            onClick={() => {
-              onSubmitHandler()
-            }}
-          />
-        ) : null}
-      </Form>
-    </Container>
+    <Segment.Group size="large">
+      <Header as="h2" content={`${sectionTitle}`} />
+      {Object.values(pages).map((elements: ElementAndResponse[]) =>
+        elements.map((elementAndResponse) => <SummaryViewWrapper {...elementAndResponse} />)
+      )}
+    </Segment.Group>
   )
 }
 
--- src/containers/Application/ApplicationOverview.tsx
+++ src/containers/Application/ApplicationOverview.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Header, Loader, Message, Modal } from 'semantic-ui-react'
+import { Button, Container, Form, Header, Loader, Message, Modal } from 'semantic-ui-react'
 import { SectionSummary, Loading } from '../../components'
+import getPageElements from '../../utils/helpers/getPageElements'
 import useGetResponsesAndElementState from '../../utils/hooks/useGetResponsesAndElementState'
 import useLoadApplication from '../../utils/hooks/useLoadApplication'
 import { useRouter } from '../../utils/hooks/useRouter'
@@ -8,7 +9,7 @@ import useUpdateApplication from '../../utils/hooks/useUpdateApplication'
 import { SectionElementStates } from '../../utils/types'
 
 const ApplicationOverview: React.FC = () => {
-  const [elementsInSections, setElementsInSections] = useState<SectionElementStates[]>()
+  const [sectionsPages, setSectionsAndElements] = useState<SectionElementStates[]>()
   const { query, push } = useRouter()
   const { serialNumber } = query
 
@@ -33,19 +34,26 @@ const ApplicationOverview: React.FC = () => {
 
   useEffect(() => {
     if (!responsesLoading && elementsState && responsesFullByCode) {
-      // Create the array of sections with array of section's element & responses
+      // Create the sections and pages structure to display each section's element & responses
       const sectionsAndElements: SectionElementStates[] = templateSections
         .sort((a, b) => a.index - b.index)
         .map((section) => {
-          return { section, elements: [] }
+          const pageNumbers = Array.from(Array(section.totalPages).keys(), (n) => n + 1)
+          const pages = pageNumbers.map((pageNumber) => {
+            const pageElements = getPageElements({
+              elementsState,
+              sectionIndex: section.index,
+              pageNumber,
+            })
+            return pageElements.map((element) => ({
+              element,
+              response: responsesFullByCode[element.code],
+            }))
+          })
+          return { sectionTitle: section.title, pages }
         })
 
-      Object.values(elementsState).forEach((element) => {
-        const response = responsesFullByCode[element.code]
-        const elementAndValue = { element, value: response ? response : null }
-        sectionsAndElements[element.sectionIndex].elements.push(elementAndValue)
-      })
-      setElementsInSections(sectionsAndElements)
+      setSectionsAndElements(sectionsAndElements)
     }
   }, [elementsState, responsesLoading])
 
@@ -55,14 +63,18 @@ const ApplicationOverview: React.FC = () => {
     <Loading />
   ) : submitError ? (
     <Message error header="Problem to submit application" list={[submitError]} />
-  ) : serialNumber && elementsInSections ? (
+  ) : serialNumber && appStatus && sectionsPages ? (
     <Container>
-      <SectionSummary
-        sectionsAndElements={elementsInSections}
-        onSubmitHandler={() => submit()}
-        appStatus={appStatus}
-      />
-      {showProcessingModal(processing, submitted)}
+      <Header as="h1" content="REVIEW AND SUBMIT" />
+      <Form>
+        {sectionsPages.map((sectionPages) => (
+          <SectionSummary sectionPages={sectionPages} editable={appStatus.status === 'DRAFT'} />
+        ))}
+        {appStatus.status === 'DRAFT' ? (
+          <Button content="Submit application" onClick={() => submit()} />
+        ) : null}
+        {showProcessingModal(processing, submitted)}
+      </Form>
     </Container>
   ) : (
     <Message error header="Problem to load application overview" />
