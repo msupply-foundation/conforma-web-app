766389943bf46a4721e65151a6ff560a95f6a24e
nmadruga <katinachovansky@gmail.com>
2020-12-01 10:31:09 +1300

    Styling change for fromPlugins summaryView

--- src/formElementPlugins/shortText/src/SummaryView.tsx
+++ src/formElementPlugins/shortText/src/SummaryView.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Header, Icon } from 'semantic-ui-react'
+import { Grid, Header, Icon } from 'semantic-ui-react'
 import { SummaryViewProps } from '../../types'
 
 const SummaryView: React.FC<SummaryViewProps> = ({ parameters, response }) => {
   return (
-    <>
-      <Header as="h3" content={parameters.label} />
-      <p>
-        {!response?.isValid ? <Icon name="exclamation circle" color="red" /> : null}
-        {response?.text}
-      </p>
-    </>
+    <Grid columns={2}>
+      <Grid.Row>
+        <Grid.Column floated="left" width={12}>
+          <Header as="h3" content={parameters.label} />
+          <p>{response?.text}</p>
+        </Grid.Column>
+        <Grid.Column floated="right" width={3}>
+          {!response?.isValid ? <Icon name="exclamation circle" color="red" /> : null}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   )
 }
 
