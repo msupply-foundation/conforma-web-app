f767b6592749d65d856e91eee1b9f1a42f50d939
nmadruga <katinachovansky@gmail.com>
2020-12-08 14:55:10 +1300

    Add constants to replace strings

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,13 @@
-const PREVIOUS = 'Previous'
-const NEXT = 'Next'
+const BUTTON_NEXT = 'Next'
+const BUTTON_PREVIOUS = 'Previous'
+const BUTTON_SUBMIT = 'Submit application'
+const TITLE_COMPANY_PLACEHOLDER = 'ABC Company'
+const TITLE_PROGRESS = 'Introduction'
 
 export {
-    PREVIOUS,
-    NEXT
+    BUTTON_NEXT,
+    BUTTON_PREVIOUS,
+    BUTTON_SUBMIT,
+    TITLE_COMPANY_PLACEHOLDER,
+    TITLE_PROGRESS
 }
