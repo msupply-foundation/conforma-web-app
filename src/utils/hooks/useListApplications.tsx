1b42a7bd2fb6147bf6189b5300adfe60822ad6fd
nmadruga <katinachovansky@gmail.com>
2020-12-10 12:32:11 +1300

    Update list to get stage after change on getApplications query and skip showing applications with problems

--- src/utils/hooks/useListApplications.tsx
+++ src/utils/hooks/useListApplications.tsx
@@ -3,7 +3,6 @@ import {
   Application,
   ApplicationStageHistory,
   GetApplicationsQuery,
-  Template,
   useGetApplicationsQuery,
 } from '../../utils/generated/graphql'
 import { ApplicationDetails } from '../types'
@@ -29,22 +28,14 @@ const useListApplication = () => {
     }
 
     const applicationsList = data?.applications?.nodes as Application[]
-    const applicationsDetails: ApplicationDetails[] = applicationsList.map((application) => {
-      const { id, serial, name, stage, status, outcome, template } = application
-      const { id: stageId } = application.applicationStageHistories
-        .nodes[0] as ApplicationStageHistory
-
-      return {
-        id,
-        type: template?.name as string,
-        isLinear: template?.isLinear as boolean,
-        serial: serial as string,
-        name: name as string,
-        stageId: stageId,
-        status: status as string,
-        outcome: outcome as string,
-      }
-    })
+    const applicationsDetails: ApplicationDetails[] = applicationsList.reduce(
+      (validApplications: ApplicationDetails[], application) => {
+        const applicationDetails = checkForValidApplication(application)
+        if (applicationDetails) return [...validApplications, applicationDetails]
+        return validApplications
+      },
+      []
+    )
     setApplications(applicationsDetails)
     setLoading(false)
   }, [data, apolloError])
@@ -59,11 +50,52 @@ const useListApplication = () => {
 function checkForApplicationsErrors(data: GetApplicationsQuery | undefined) {
   if (!data) return 'No data'
   if (!data.applications) return 'Unexpected list applications result'
+
+  // const problemsInApplications: string[] = []
   const numberOfApplications = data.applications.nodes.length as number
   if (numberOfApplications === 0) return 'No Applications found'
-  if (data.applications.nodes.some((application) => !application?.template))
-    return 'Unexpected template missing in one or more applications'
+  data.applications.nodes.forEach((application) => {
+    const { id, template } = application as Application
+    if (!template) console.log(`Application id ${id} is missing a template link`)
+    // if (!template) problemsInApplications.push(`Application id ${id} is missing a template link`)
+  })
+  data?.applications.nodes.forEach((application) => {
+    const { id, applicationStageHistories } = application as Application
+    if (!applicationStageHistories || applicationStageHistories.nodes.length === 0)
+      console.log(`Application id ${id} is missing a template link`)
+    // problemsInApplications.push(`Application id ${id} is missing the current stage`)
+  })
+  // if (problemsInApplications.length > 0) return JSON.stringify(problemsInApplications)
   return null
+}
+
+function checkForValidApplication(application: Application): ApplicationDetails | null {
+  const { id, serial, name, status, outcome, template, applicationStageHistories } = application
+  const stages = application.applicationStageHistories.nodes as ApplicationStageHistory[]
+
+  if (
+    !id ||
+    !serial ||
+    !name ||
+    !status ||
+    !outcome ||
+    !template ||
+    !applicationStageHistories ||
+    stages.length === 0
+  )
+    return null
+
+  const { id: stageId } = stages[0]
+  return {
+    id,
+    type: template?.name as string,
+    isLinear: template?.isLinear as boolean,
+    serial: serial as string,
+    name: name as string,
+    stageId: stageId,
+    status: status as string,
+    outcome: outcome as string,
+  }
 }
 
 export default useListApplication
