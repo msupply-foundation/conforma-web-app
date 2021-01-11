4fd38e33a18c08472714ce996e1c31a257c451b1
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-01-11 13:10:54 +1300

    Applicant summary default to Open

--- src/components/Application/SectionSummary.tsx
+++ src/components/Application/SectionSummary.tsx
@@ -19,7 +19,7 @@ const SectionSummary: React.FC<SectionSummaryProps> = ({
   canEdit,
 }) => {
   const { section, pages } = sectionPages
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(true)
 
   const handleClick = () => {
     setIsOpen(!isOpen)
