dcfe08c4298f02708d40dbea69c328edeb13f814
nmadruga <katinachovansky@gmail.com>
2021-01-13 23:17:15 +1300

    Set user roles based on permissions user have for template

--- src/containers/Application/ApplicationList.tsx
+++ src/containers/Application/ApplicationList.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Table, List, Label } from 'semantic-ui-react'
 import { Loading, FilterList } from '../../components'
 import { useRouter } from '../../utils/hooks/useRouter'
 import { Link } from 'react-router-dom'
 import useListApplication from '../../utils/hooks/useListApplications'
 import strings from '../../utils/constants'
+import findUserRole from '../../utils/helpers/translations/findUserRole'
+import { useUserState } from '../../contexts/UserState'
 
 const ApplicationList: React.FC = () => {
-  const { query } = useRouter()
+  const { query, push } = useRouter()
   const { type, userRole } = query
+  const {
+    userState: { templatePermissions },
+  } = useUserState()
   const { error, loading, applications } = useListApplication({ type })
+
+  useEffect(() => {
+    if (type && !userRole) {
+      const found = Object.entries(templatePermissions).find(([template]) => template === type)
+      if (found) {
+        const [template, permissions] = found
+        const newRole = findUserRole(permissions)
+        // TODO: Call helper to build similar URL query with the new userRole
+        if (newRole) push(`/applications?type=${type}&user-role=${newRole}`)
+      }
+    }
+  }, [type, userRole])
 
   return error ? (
     <Label content={strings.ERROR_APPLICATIONS_LIST} error={error} />
@@ -24,7 +41,7 @@ const ApplicationList: React.FC = () => {
           <List.Item key={`app_selected_parameter_${value}`} content={key + ' : ' + value} />
         ))}
       </List>
-      {applications && applications.length > 0 && (
+      {userRole && applications && applications.length > 0 && (
         // TODO: Implement sortable columns -> Requires reducer
         <Table sortable stackable selectable>
           <Table.Header>
---
+++ src/utils/helpers/translations/findUserRole.ts
@@ -0,0 +1,25 @@
+import { PermissionPolicyType } from '../../generated/graphql'
+
+interface UserRoles {
+  [role: string]: Array<PermissionPolicyType>
+}
+
+const userRoles: UserRoles = {
+  applicant: [PermissionPolicyType.Apply],
+  reviewer1: [PermissionPolicyType.Review],
+  reviewer2: [PermissionPolicyType.Review, PermissionPolicyType.Assign],
+  supervisor: [PermissionPolicyType.Assign],
+  consolidator: [PermissionPolicyType.Assign], //, PermissionPolicyType.Consolidate]
+}
+
+export default (permissions: Array<PermissionPolicyType>): string | undefined => {
+  const comparePermissions = permissions.map((permissionType) => permissionType.toUpperCase())
+
+  // Compare array of permission checking if are the same
+  const matchingRoles = Object.keys(userRoles).filter((type) => {
+    const difference = userRoles[type].filter((x) => !comparePermissions.includes(x))
+    return difference.length === 0
+  })
+  console.log('Possible user-roles:', matchingRoles)
+  return matchingRoles.length > 0 ? matchingRoles[0] : undefined
+}
