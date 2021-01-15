c380cde68b0ee20fb056b1c2a425abf140786fd9
nmadruga <nmadruga@users.noreply.github.com>
2021-01-15 13:18:52 +1300

    Rename method
    Co-authored-by: Carl Smith <5456533+CarlosNZ@users.noreply.github.com>

--- src/utils/hooks/useRouter.tsx
+++ src/utils/hooks/useRouter.tsx
@@ -35,7 +35,7 @@ interface RouterResult {
   location: any
 }
 
-const replaceHifenKeys = (parsedQuery: { [key: string]: any }) => {
+const replaceSnakeCaseKeys = (parsedQuery: { [key: string]: any }) => {
   if (Object.keys(parsedQuery).length === 0) return parsedQuery
   const replacedHifenKeys = Object.keys(parsedQuery).map((key) => {
     const convertedKey = key.replace(/-([a-z])/g, function (m, w) {
