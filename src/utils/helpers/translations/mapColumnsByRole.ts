881b31dd29d3f55558a4fb36e0310149e64f2780
nmadruga <katinachovansky@gmail.com>
2021-01-15 13:06:14 +1300

    Adding function comment on file mapColumnsByRole

--- src/utils/helpers/translations/mapColumnsByRole.ts
+++ src/utils/helpers/translations/mapColumnsByRole.ts
@@ -1,3 +1,13 @@
+/**
+ * @function: mapColumnsByRole
+ * Define a object with headers details from the current user Role to interact with applications.
+ * Each entry in the headers object will define the header title, possible filter keys and a render
+ * function (To be done in another issue) to return a component to be rendered in the cell of
+ * the columns for this header = using the application data received in params.
+ * - @param userRole - Current user role - as string (TODO: Use type)
+ * - @returns Object with each Column details to construct header and rows of applications list.s
+ */
+
 export enum APPLICATION_COLUMN {
   LAST_ACTIVE_DATE = 'Last active date',
   DEADLINE_DATE = 'Deadline date',
