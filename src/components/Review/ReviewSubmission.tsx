f7772d78bf2d301c8cd68e48f9fbc84d76c0a6b9
nmadruga <katinachovansky@gmail.com>
2021-01-21 13:06:04 +1300

    Dummy Review submission page

---
+++ src/components/Review/ReviewSubmission.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Segment } from 'semantic-ui-react'
+
+const ReviewSubmission: React.FC = () => {
+  return (
+    <Segment>
+      <p>REVIEW SUBMITED</p>
+    </Segment>
+  )
+}
+
+export default ReviewSubmission
