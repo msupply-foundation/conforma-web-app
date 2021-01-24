6c783112aa5d0b2e9465e158ec6c20106a7f92ce
nmadruga <katinachovansky@gmail.com>
2021-01-25 12:57:26 +1300

    Add new hook to get Application status - after trigger has finished running

--- src/utils/hooks/useLoadApplication.tsx
+++ src/utils/hooks/useLoadApplication.tsx
@@ -4,8 +4,10 @@ import {
   ApplicationStageStatusAll,
   TemplateStage,
   useGetApplicationQuery,
+  useGetApplicationStatusQuery,
 } from '../../utils/generated/graphql'
 import useTriggerProcessing from '../../utils/hooks/useTriggerProcessing'
+import getApplicationStatusQuery from '../graphql/queries/getApplicationStatus.query'
 import { getApplicationSections } from '../helpers/application/getSectionsPayload'
 import {
   ApplicationDetails,
@@ -19,21 +21,33 @@ const useLoadApplication = ({ serialNumber, networkFetch }: UseGetApplicationPro
   const [templateSections, setSections] = useState<TemplateSectionPayload[]>([])
   const [appStages, setAppStages] = useState<ApplicationStages>()
   const [isApplicationLoaded, setIsApplicationLoaded] = useState(false)
-
-  const { triggerProcessing, error: triggerError } = useTriggerProcessing({
-    serialNumber,
-    // triggerType: 'applicationTrigger',
-  })
+  const [checkTrigger, setCheckTrigger] = useState(false)
+  const [applicationError, setApplicationError] = useState('')
 
   const { data, loading, error } = useGetApplicationQuery({
     variables: {
       serial: serialNumber,
     },
-    skip: triggerProcessing || isApplicationLoaded,
+    skip: isApplicationLoaded,
     fetchPolicy: networkFetch ? 'network-only' : 'cache-first',
   })
 
+  const { triggerProcessing, error: triggerError } = useTriggerProcessing({
+    checkTrigger,
+    serialNumber,
+    // triggerType: 'applicationTrigger',
+  })
+
+  const { data: data2, loading: loading2, error: error2 } = useGetApplicationStatusQuery({
+    variables: { serial: serialNumber },
+    skip: !application || triggerProcessing,
+  })
+
   useEffect(() => {
+    if (error) {
+      setApplicationError(error.message)
+    }
+
     if (data?.applicationBySerial) {
       const application = data.applicationBySerial as Application
 
@@ -44,19 +58,6 @@ const useLoadApplication = ({ serialNumber, networkFetch }: UseGetApplicationPro
         serial: application.serial as string,
         name: application.name as string,
         outcome: application.outcome as string,
-      }
-
-      if (data?.applicationStageStatusAlls && data?.applicationStageStatusAlls.nodes.length > 0) {
-        const stages = data.applicationStageStatusAlls.nodes as ApplicationStageStatusAll[]
-        if (stages.length > 1)
-          console.log('StageStatusAll More than one results for 1 application!')
-        const { stageId, stage, status, statusHistoryTimeCreated } = stages[0] // Should only have one result
-        applicationDetails.stage = {
-          id: stageId as number,
-          name: stage as string,
-          status: status as string,
-          date: statusHistoryTimeCreated.split('T')[0],
-        }
       }
 
       setApplication(applicationDetails)
@@ -75,9 +76,38 @@ const useLoadApplication = ({ serialNumber, networkFetch }: UseGetApplicationPro
         submissionMessage: application.template?.submissionMessage as string,
       })
 
-      setIsApplicationLoaded(true)
+      setCheckTrigger(true)
+      // setIsApplicationLoaded(true)
     }
   }, [data, loading, error])
+
+  useEffect(() => {
+    if (error2) {
+      setApplicationError(error2.message)
+    }
+
+    if (
+      application &&
+      data2?.applicationStageStatusAlls &&
+      data2?.applicationStageStatusAlls.nodes.length > 0
+    ) {
+      const stages = data2.applicationStageStatusAlls.nodes as ApplicationStageStatusAll[]
+      if (stages.length > 1) console.log('StageStatusAll More than one results for 1 application!')
+      const { stageId, stage, status, statusHistoryTimeCreated } = stages[0] // Should only have one result
+      setApplication({
+        ...application,
+        stage: {
+          id: stageId as number,
+          name: stage as string,
+          status: status as string,
+          date: statusHistoryTimeCreated.split('T')[0],
+        },
+      })
+      console.log('Status', status, 'Trigger', triggerProcessing)
+
+      setIsApplicationLoaded(true)
+    }
+  }, [data2, error2, triggerProcessing])
 
   return {
     error: error || triggerError,
