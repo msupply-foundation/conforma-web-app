4263a76c490cd9d17199bff35742dff4ae1ad081
nmadruga <katinachovansky@gmail.com>
2021-02-16 11:20:10 +1300

    Change reduce to use filter and map to get assigned questions

--- src/utils/helpers/review/getAssignedQuestions.ts
+++ src/utils/helpers/review/getAssignedQuestions.ts
@@ -3,7 +3,6 @@ import {
   ReviewQuestionAssignment,
   TemplateElement,
 } from '../../generated/graphql'
-import { ReviewQuestion } from '../../types'
 interface GetAssignedQuestionsProps {
   reviewQuestions: ReviewQuestionAssignment[]
 }
@@ -15,29 +14,23 @@ interface GetAssignedQuestionsProps {
  * @param reviewQuestions GraphQL array of assigned questions
  */
 const getAssignedQuestions = ({ reviewQuestions }: GetAssignedQuestionsProps) => {
-  const assignedQuestions = reviewQuestions.reduce(
-    (validQuestionAssignments: ReviewQuestion[], questionAssignment) => {
+  const assignedQuestions = reviewQuestions
+    .filter((questionAssignment) => {
+      // Remove review_response without a response link
+      const { templateElement } = questionAssignment
+      const { applicationResponses } = templateElement as TemplateElement
+      return applicationResponses.nodes.length > 0
+    })
+    .map((questionAssignment) => {
+      // Map each review_response to ReviewQuestion object
       const { templateElement } = questionAssignment
       const { code, section, applicationResponses } = templateElement as TemplateElement
-      const currentResponse =
-        applicationResponses.nodes.length > 0
-          ? (applicationResponses.nodes[0] as ApplicationResponse)
-          : undefined
-
-      // Check if assigned question is valid to include in array
-      if (!currentResponse) return validQuestionAssignments
-
-      return [
-        ...validQuestionAssignments,
-        {
-          code,
-          responseId: currentResponse.id,
-          sectionIndex: section?.index as number,
-        },
-      ]
-    },
-    []
-  )
+      return {
+        code,
+        responseId: (applicationResponses.nodes[0] as ApplicationResponse).id,
+        sectionIndex: section?.index as number,
+      }
+    })
   return assignedQuestions
 }
 
