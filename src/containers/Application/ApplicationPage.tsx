c68f8be0d594d08b692fe6bab013190151eee41c
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-02-23 12:40:55 +1300

    Handle re-directs

--- src/containers/Application/ApplicationPage.tsx
+++ src/containers/Application/ApplicationPage.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FullStructure } from '../../utils/types'
+import useGetFullApplicationStructure from '../../utils/hooks/useGetFullApplicationStructure'
+import { ApplicationStatus } from '../../utils/generated/graphql'
+import { useRouter } from '../../utils/hooks/useRouter'
 
 interface ApplicationProps {
   structure: FullStructure
 }
 
 const ApplicationPage: React.FC<ApplicationProps> = ({ structure }) => {
+  const { error, isLoading, fullStructure, responsesByCode } = useGetFullApplicationStructure(
+    structure
+  )
+
+  const {
+    push,
+    match: { path },
+  } = useRouter()
+
+  console.log('Structure', fullStructure)
+
+  useEffect(() => {
+    if (!structure) return
+
+    console.log('Structure', fullStructure)
+
+    // Re-direct based on application status and progress
+    if (structure.info.current?.status === ApplicationStatus.ChangesRequired)
+      push(`/application/${structure.info.serial}`)
+    // if (structure.info.current?.status !== ApplicationStatus.Draft)
+    //   push(`/application/${structure.info.serial}/summary`)
+
+    // TO-DO: Redirect based on Progress (wait till Progress calculation is done)
+  }, [structure])
+
   return <p>IN PROGRESS PAGE</p>
 }
 
