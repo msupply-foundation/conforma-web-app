339792e1eb0c4b4d954ea9fa4d9807a40d60f068
nmadruga <katinachovansky@gmail.com>
2021-02-24 16:23:19 +1300

    build progress for pages and sections as pages combined progress

--- src/utils/helpers/structure/generateProgress.ts
+++ src/utils/helpers/structure/generateProgress.ts
@@ -1,23 +1,62 @@
-import { ElementStateNEW, FullStructure, Progress } from '../../types'
+import { TemplateElementCategory } from '../../generated/graphql'
+import { ElementStateNEW, FullStructure, PageNEW, Progress } from '../../types'
+
+const initialProgress = {
+  doneNonRequired: 0,
+  doneRequired: 0,
+  completed: false,
+  totalNonRequired: 0,
+  totalRequired: 0,
+  totalSum: 0,
+  valid: true,
+}
+
+const calculateCompleted = (totalSum: number, doneRequired: number, doneNonRequired: number) => {
+  const totalDone = doneRequired + doneNonRequired
+  return totalSum === totalDone
+}
+
+const getSectionProgress = (pages: PageNEW[]): Progress => {
+  return pages.reduce((sectionProgress: Progress, { progress }) => {
+    if (!progress) return sectionProgress
+    let {
+      doneNonRequired,
+      doneRequired,
+      completed,
+      totalNonRequired,
+      totalRequired,
+      totalSum,
+      valid,
+    } = sectionProgress
+    doneNonRequired += progress.doneNonRequired || 0
+    totalNonRequired += progress.totalNonRequired || 0
+    doneRequired += progress.doneRequired || 0
+    totalRequired += progress.totalRequired || 0
+    totalSum = totalRequired + doneNonRequired
+    completed = calculateCompleted(totalSum, doneRequired, doneNonRequired)
+    if (!progress.valid) valid = false
+    return {
+      doneNonRequired,
+      doneRequired,
+      completed,
+      totalNonRequired,
+      totalRequired,
+      totalSum,
+      valid,
+    }
+  }, initialProgress)
+}
 
 export const generateResponsesProgress = (structure: FullStructure) => {
-  Object.entries(structure.sections).forEach(([code, section]) => {
-    Object.entries(section.pages).forEach(([pageName, page]) => {
-      const progress: Progress = {
-        doneNonRequired: 0,
-        doneRequired: 0,
-        completed: false,
-        totalNonRequired: 0,
-        totalRequired: 0,
-        totalSum: 0,
-        valid: true,
-      }
+  Object.values(structure.sections).forEach((section) => {
+    Object.values(section.pages).forEach((page) => {
+      const progress: Progress = initialProgress
       page.state
         .filter(({ element }) => {
-          const { isVisible, isEditable } = element as ElementStateNEW
-          return isVisible && isEditable
+          const { category, isVisible, isEditable } = element as ElementStateNEW
+          return isVisible && isEditable && category === TemplateElementCategory.Question
         })
-        .forEach(({ element, response, review }) => {
+        .forEach(({ element, response }) => {
           const { isRequired } = element as ElementStateNEW
           if (isRequired) progress.totalRequired++
           else progress.totalNonRequired++
@@ -25,8 +64,19 @@ export const generateResponsesProgress = (structure: FullStructure) => {
             if (isRequired) progress.doneRequired++
             else progress.doneNonRequired++
           }
-          if (response !== null && !response.isValid) progress.valid = false
+          if (response !== null && !response?.isValid) progress.valid = false
+          progress.totalSum = progress.totalRequired + progress.doneNonRequired
+          progress.completed = calculateCompleted(
+            progress.totalSum,
+            progress.doneRequired,
+            progress.doneNonRequired
+          )
         })
+      page.progress = progress
     })
+    section.progress = getSectionProgress(Object.values(section.pages))
+    section.invalidPage =
+      Object.values(section.pages).find(({ progress }) => progress && !progress.valid)?.number ||
+      undefined
   })
 }
