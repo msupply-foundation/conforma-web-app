be8493239000156757ec02ff318e5ced81badc88
nmadruga <katinachovansky@gmail.com>
2021-02-25 22:26:22 +1300

    Show summary button when all sections ar4e completed

--- src/containers/Application/ApplicationHome.tsx
+++ src/containers/Application/ApplicationHome.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Button, Divider, Header, Message, Segment, Sticky } from 'semantic-ui-react'
 import { FullStructure, SectionAndPage, TemplateDetails } from '../../utils/types'
 import useGetFullApplicationStructure from '../../utils/hooks/useGetFullApplicationStructure'
 import { ApplicationHeader, Loading } from '../../components'
-import { Button, Divider, Header, Message, Segment } from 'semantic-ui-react'
 import strings from '../../utils/constants'
 import { useUserState } from '../../contexts/UserState'
 import SectionsProgress from '../../components/Sections/SectionsProgress'
@@ -21,24 +21,50 @@ const ApplicationHome: React.FC<ApplicationProps> = ({ structure, template }) =>
   const {
     userState: { currentUser },
   } = useUserState()
+
   const { error, fullStructure } = useGetFullApplicationStructure({
     structure,
   })
-
-  if (!fullStructure || !fullStructure.responsesByCode) return <Loading />
 
   const handleResumeClick = ({ sectionCode, pageName }: SectionAndPage) => {
     push(`/applicationNEW/${serialNumber}/${sectionCode}/${pageName}`)
   }
 
+  const handleSummaryClicked = () => {
+    push(`/applicationNEW/${serialNumber}/summary`)
+  }
+
+  if (!fullStructure || !fullStructure.responsesByCode) return <Loading />
+
+  const isCompleted = Object.values(fullStructure.sections).every(
+    ({ progress }) => progress?.completed && progress.valid
+  )
+
   const HomeMain: React.FC = () => {
     return (
-      <Segment>
-        <Header as="h5">{strings.SUBTITLE_APPLICATION_STEPS}</Header>
-        <Header as="h5">{strings.TITLE_STEPS.toUpperCase()}</Header>
-        <SectionsProgress sections={fullStructure.sections} resumeApplication={handleResumeClick} />
-        <Divider />
-      </Segment>
+      <>
+        <Segment>
+          <Header as="h5">{strings.SUBTITLE_APPLICATION_STEPS}</Header>
+          <Header as="h5">{strings.TITLE_STEPS.toUpperCase()}</Header>
+          <SectionsProgress
+            sections={fullStructure.sections}
+            resumeApplication={handleResumeClick}
+          />
+          <Divider />
+        </Segment>
+        {isCompleted && (
+          <Sticky
+            pushing
+            style={{ backgroundColor: 'white', boxShadow: ' 0px -5px 8px 0px rgba(0,0,0,0.1)' }}
+          >
+            <Segment basic textAlign="right">
+              <Button color="blue" onClick={handleSummaryClicked}>
+                {strings.BUTTON_SUMMARY}
+              </Button>
+            </Segment>
+          </Sticky>
+        )}
+      </>
     )
   }
 
@@ -50,18 +76,3 @@ const ApplicationHome: React.FC<ApplicationProps> = ({ structure, template }) =>
 }
 
 export default ApplicationHome
-
-{
-  /* {isApplicationCompleted && (
-        <Sticky
-          pushing
-          style={{ backgroundColor: 'white', boxShadow: ' 0px -5px 8px 0px rgba(0,0,0,0.1)' }}
-        >
-          <Segment basic textAlign="right">
-            <Button color="blue" onClick={setSummaryButtonClicked}>
-              {strings.BUTTON_SUMMARY}
-            </Button>
-          </Segment>
-        </Sticky>
-      )} */
-}
