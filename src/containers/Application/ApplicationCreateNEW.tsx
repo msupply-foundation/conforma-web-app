bc08e4128c9ac05a2bd3325ce3e2623e28f15232
nmadruga <katinachovansky@gmail.com>
2021-03-03 11:25:21 +1300

    Create new useLoadTemplate hook to be used in new ApplicationCreate container

--- src/containers/Application/ApplicationCreateNEW.tsx
+++ src/containers/Application/ApplicationCreateNEW.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useApplicationState } from '../../contexts/ApplicationState'
+import { useUserState } from '../../contexts/UserState'
+import useLoadTemplate from '../../utils/hooks/useLoadTemplateNEW'
+import { useRouter } from '../../utils/hooks/useRouter'
 
 const ApplicationCreateNEW: React.FC = () => {
+  const { applicationState, setApplicationState } = useApplicationState()
+  const { serialNumber } = applicationState
+  const { push, query } = useRouter()
+  const { type } = query
+
+  const { error, loading, template, sectionsStructure, elementsIds } = useLoadTemplate({
+    templateCode: type as string,
+  })
+
+  const {
+    userState: { currentUser },
+  } = useUserState()
+
+  useEffect(() => {
+    if (sectionsStructure) console.log(sectionsStructure)
+  }, [sectionsStructure])
+
   return <p>APPLICATION CREATE</p>
 }
 
---
+++ src/utils/hooks/useLoadTemplateNEW.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react'
+import {
+  GetTemplateQuery,
+  Template,
+  TemplateSection,
+  useGetTemplateQuery,
+} from '../generated/graphql'
+import { buildTemplateSectionsStructure } from '../helpers/structure/buildSectionsStructureNEW'
+import { getTemplateSections } from '../helpers/application/getSectionsDetails'
+import { SectionsStructureNEW, TemplateDetails } from '../types'
+
+interface useLoadTemplateProps {
+  templateCode: string
+}
+
+const useLoadTemplate = (props: useLoadTemplateProps) => {
+  const { templateCode } = props
+  const [template, setTemplate] = useState<TemplateDetails>()
+  const [sectionsStructure, setSectionsStructure] = useState<SectionsStructureNEW>()
+  const [elementsIds, setElementsIds] = useState<number[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  const { data, loading: apolloLoading, error: apolloError } = useGetTemplateQuery({
+    variables: {
+      code: templateCode,
+    },
+  })
+
+  useEffect(() => {
+    // Check that only one tempalte matched
+    let error = checkForTemplateErrors(data)
+    if (error) {
+      setError(error)
+      setLoading(false)
+      return
+    }
+
+    const template = data?.templates?.nodes[0] as Template
+
+    error = checkForTemplatSectionErrors(template)
+    if (error) {
+      setError(error)
+      setLoading(false)
+      return
+    }
+
+    const { id, code, name, startMessage } = template
+
+    setTemplate({
+      id,
+      code,
+      name: name as string,
+      startMessage: startMessage ? startMessage : undefined,
+    })
+
+    const templateSections = template.templateSections.nodes as TemplateSection[]
+    const sections = getTemplateSections(templateSections)
+    const sectionsStructure = buildTemplateSectionsStructure(sections)
+    setSectionsStructure(sectionsStructure)
+
+    const elements = [] as number[]
+
+    templateSections.forEach((section) => {
+      const { templateElementsBySectionId } = section as TemplateSection
+      templateElementsBySectionId.nodes.forEach((element) => {
+        if (element?.id && element.category === 'QUESTION') elements.push(element.id)
+      })
+    })
+    setElementsIds(elements)
+
+    setLoading(false)
+  }, [data])
+
+  return {
+    loading: apolloLoading || loading,
+    error: apolloError?.message || error,
+    template,
+    sectionsStructure,
+    elementsIds,
+  }
+}
+
+function checkForTemplateErrors(data: GetTemplateQuery | undefined) {
+  if (data?.templates?.nodes?.length === null) return 'Unexpected template result'
+  const numberOfTemplates = data?.templates?.nodes.length as number
+  if (numberOfTemplates === 0) return 'Template not found'
+  if (numberOfTemplates > 1) return 'More then one template found'
+  return null
+}
+
+function checkForTemplatSectionErrors(template: Template) {
+  if (template?.templateSections?.nodes === null) return 'Unexpected template section result'
+  const numberOfSections = template?.templateSections?.nodes.length as number
+  if (numberOfSections === 0) return 'No template sections'
+  return null
+}
+
+export default useLoadTemplate
