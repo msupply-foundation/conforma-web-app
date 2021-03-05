0a0c1529b74cceccb97126709b16677ba3b7a355
andreievg <andrei.evguenov@gmail.com>
2021-03-05 20:14:35 +1300

    update test

--- src/components/Review/ReviewPageWrapperTest.tsx
+++ src/components/Review/ReviewPageWrapperTest.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Header } from 'semantic-ui-react'
 import { Loading } from '../../components'
 import ReviewPage from '../../containers/Review/ReviewPage'
+import { ReviewStatus } from '../../utils/generated/graphql'
 import useGetFullReviewStructure from '../../utils/hooks/useGetFullReviewStructure'
 import { FullStructure } from '../../utils/types'
 
 const ReviewPageWrapperTest: React.FC<{ structure: FullStructure }> = ({ structure }) => {
-  return <ReviewPage structure={structure} reviewAssignmentId={1020} />
+  return (
+    <ReviewPage
+      structure={structure}
+      reviewAssignment={{ id: 1020, reviews: { nodes: [{ status: ReviewStatus.Draft }] } }}
+    />
+  )
 }
 
 const ReviewPageTest: React.FC<{
