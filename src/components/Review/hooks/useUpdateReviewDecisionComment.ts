16ec8222ab2e5e2970330144f0f147b5c1bd5752
andreievg <andrei.evguenov@gmail.com>
2021-03-08 23:01:02 +1300

    logic to add recommended applicant visibility to review response

--- src/components/Review/hooks/useUpdateReviewDecisionComment.ts
+++ src/components/Review/hooks/useUpdateReviewDecisionComment.ts
@@ -1,14 +1,21 @@
-import { useUpdateReviewDecisionMutation } from '../../../utils/generated/graphql'
+import { FetchResult } from '@apollo/client'
+import {
+  UpdateReviewDecisionMutation,
+  useUpdateReviewDecisionMutation,
+} from '../../../utils/generated/graphql'
 
+// below type is constructred by using suggestion
+type promiseReturnType = Promise<
+  FetchResult<UpdateReviewDecisionMutation, Record<string, any>, Record<string, any>>
+>
 // hook used to update review decision comment, as per type definition below (returns promise that resolve with mutation result data)
-type UseSubmitReview = (reviewDecisionId: number) => (comment: string) => Promise<void>
+type UseSubmitReview = (reviewDecisionId: number) => (comment: string) => promiseReturnType
 
 const useUpdateReviewDecisionComment: UseSubmitReview = (reviewDecisionId) => {
   const [updateReviewDecision] = useUpdateReviewDecisionMutation()
 
-  return async (comment: string) => {
+  return async (comment: string) =>
     updateReviewDecision({ variables: { reviewDecisionId, data: { comment } } })
-  }
 }
 
 export default useUpdateReviewDecisionComment
---
+++ src/components/Review/hooks/useUpdateReviewResponse.ts
@@ -0,0 +1,60 @@
+import { FetchResult } from '@apollo/client'
+import {
+  ReviewResponse,
+  ReviewResponseDecision,
+  ReviewResponseRecommendedApplicantVisibility,
+  UpdateReviewResponseMutation,
+  useUpdateReviewResponseMutation,
+} from '../../../utils/generated/graphql'
+
+// below type is constructred by using suggestion
+type promiseReturnType = Promise<
+  FetchResult<UpdateReviewResponseMutation, Record<string, any>, Record<string, any>>
+>
+
+// hook used to update review response, comment and decision, use as per type definition belo
+// also computes and updates recommendedApplicantVisibility
+type useUpdateReviewResponse = (
+  reviewResponseId: number
+) => (reviewResponse: ReviewResponse) => promiseReturnType
+
+const useUpdateReviewResponse: useUpdateReviewResponse = (reviewResponseId) => {
+  const [updateReviewResponse] = useUpdateReviewResponseMutation()
+
+  return async (reviewResponse) =>
+    updateReviewResponse({
+      variables: {
+        id: reviewResponseId,
+        comment: reviewResponse.comment,
+        decision: reviewResponse.decision,
+        recommendedApplicantVisibility: computeVisibility(reviewResponse),
+      },
+    })
+}
+
+// is_visible_to_applicant is set by an action on back, it uses recomended_application_visibility of the
+// last level review to determine what will be visible to application in LOQ
+// we can add a checkbox in decision modal to determine this (thus logic is placed here)
+const computeVisibility = (reviewResponse: ReviewResponse | undefined) => {
+  // would rather computer this from actual review level, but dont' want to pass too far as props or rely on context
+  if (!reviewResponse)
+    return ReviewResponseRecommendedApplicantVisibility.OriginalResponseNotVisibleToApplicant
+
+  const isLevelOne = reviewResponse.originalResponseId === reviewResponse.id
+
+  if (isLevelOne) {
+    if (reviewResponse.decision === ReviewResponseDecision.Decline)
+      return ReviewResponseRecommendedApplicantVisibility.OriginalResponseVisibleToApplicant
+
+    return ReviewResponseRecommendedApplicantVisibility.OriginalResponseNotVisibleToApplicant
+  }
+  // consolidator
+  if (
+    reviewResponse.decision === ReviewResponseDecision.Agree &&
+    reviewResponse.originalResponse?.decision === ReviewResponseDecision.Decline
+  )
+    return ReviewResponseRecommendedApplicantVisibility.OriginalResponseVisibleToApplicant
+  return ReviewResponseRecommendedApplicantVisibility.OriginalResponseNotVisibleToApplicant
+}
+
+export default useUpdateReviewResponse
