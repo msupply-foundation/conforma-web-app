b446d3a219331212e1e81e1792c20a2a04426cce
andreievg <andrei.evguenov@gmail.com>
2021-03-16 20:54:05 +1300

    add mutation and hook

---
+++ src/utils/hooks/useRestartApplication.ts
@@ -0,0 +1,49 @@
+import {
+  ApplicationPatch,
+  TemplateElementCategory,
+  Trigger,
+  useRestartApplicationMutation,
+} from '../generated/graphql'
+import { FullStructure } from '../types'
+
+// below lines are used to get return type of the function that is returned by useRestartApplicationMutation
+type UseRestartApplicationMutation = ReturnType<typeof useRestartApplicationMutation>
+type promiseReturnType = ReturnType<UseRestartApplicationMutation[0]>
+// hook used to submit review, , as per type definition below (returns promise that resolve with mutation result data)
+type UseRestartApplication = (serial: string) => (structure: FullStructure) => promiseReturnType
+
+type ConstructRestartApplicationPatch = (structure: FullStructure) => ApplicationPatch
+
+const useRestartApplication: UseRestartApplication = (serial) => {
+  const [restartApplicationMutation] = useRestartApplicationMutation()
+
+  const constructRestartApplicationPatch: ConstructRestartApplicationPatch = (structure) => {
+    const reviewableElements = Object.values(structure.elementsById || {}).filter(
+      (element) => (element.element.category = TemplateElementCategory.Question)
+    )
+
+    const applicationResponsesCreate = reviewableElements.map((element) => ({
+      templateElementId: element.element.id,
+      value: element?.latestApplicationResponse?.value,
+    }))
+
+    return {
+      trigger: Trigger.OnApplicationRestart,
+      applicationResponsesUsingId: {
+        create: applicationResponsesCreate,
+      },
+    }
+  }
+
+  const restartApplication = async (structure: FullStructure) =>
+    await restartApplicationMutation({
+      variables: {
+        serial,
+        applicationPatch: constructRestartApplicationPatch(structure),
+      },
+    })
+
+  return restartApplication
+}
+
+export default useRestartApplication
