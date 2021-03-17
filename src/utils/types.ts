adab8ed3e1d0e76c7ee5e4bb65e37f332f9d9173
andreievg <andrei.evguenov@gmail.com>
2021-03-18 12:25:37 +1300

    add types for re review

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -20,7 +20,6 @@ import { ValidationState } from '../formElementPlugins/types'
 import { IQueryNode } from '@openmsupply/expression-evaluator/lib/types'
 import { SummaryViewWrapperProps } from '../formElementPlugins/types'
 import { APPLICATION_COLUMNS, USER_ROLES } from './data'
-import { DateTime } from 'luxon'
 
 export {
   ApplicationDetails,
@@ -344,6 +343,7 @@ type PageElement = {
   previousApplicationResponse: ApplicationResponse
   latestApplicationResponse: ApplicationResponse
   thisReviewLatestResponse?: ReviewResponse
+  isNewApplicationResponse?: boolean
   review?: ReviewQuestionDecision
   assignmentId: number
   isAssigned?: boolean
@@ -474,6 +474,8 @@ interface ReviewProgress {
   totalReviewable: number
   doneConform: number
   doneNonConform: number
+  doneNewReviewable: number
+  totalNewReviewable: number
 }
 enum ReviewAction {
   canContinue = 'CAN_CONTINUE',
@@ -523,7 +525,7 @@ interface SortQuery {
 interface StageAndStatus {
   stage: ApplicationStage
   status: ApplicationStatus
-  date: DateTime
+  date: Date
 }
 
 interface StageDetails {
