754b1aa58c1042c0ac93b9416ab040d15734d6f4
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-04-01 12:01:59 +1300

    Summary View for Image display

--- src/formElementPlugins/imageDisplay/src/SummaryView.tsx
+++ src/formElementPlugins/imageDisplay/src/SummaryView.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Message } from 'semantic-ui-react'
+import { Image } from 'semantic-ui-react'
 import { SummaryViewProps } from '../../types'
 
-const SummaryView: React.FC<SummaryViewProps> = ({ parameters, Markdown }) => {
+const SummaryView: React.FC<SummaryViewProps> = ({ parameters }) => {
+  const { url, size, alignment, altText } = parameters
+
   return (
-    <Message>
-      <Message.Header>{parameters.title}</Message.Header>
-      <Markdown text={parameters.text} />
-    </Message>
+    <Image
+      src={url}
+      size="small" // Always show small in Summary view
+      // Have to do a bit of trickery in terms margin over-rides to get
+      // right-alignment working
+      centered={alignment === 'center' || alignment === 'right'}
+      style={alignment === 'right' ? { marginRight: 'unset' } : {}}
+      alt={altText}
+      title={altText}
+    />
   )
 }
 
