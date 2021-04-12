1a3776570f0802614cd91e75943e3e9994b8ceb2
nmadruga <katinachovansky@gmail.com>
2021-04-13 09:43:13 +1200

    Localisation for ApplicantActionCell

--- src/components/List/Cells/ApplicantActionCell.tsx
+++ src/components/List/Cells/ApplicantActionCell.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { ApplicationStatus } from '../../../utils/generated/graphql'
 import { CellProps } from '../../../utils/types'
+import strings from '../../../utils/constants'
 
 const ApplicantActionCell: React.FC<CellProps> = ({ application: { status, serial } }) => {
   let action = ''
 
-  if (status === ApplicationStatus.ChangesRequired) action = 'Update'
-  if (status === ApplicationStatus.Draft) action = 'Continue'
+  if (status === ApplicationStatus.ChangesRequired) action = strings.ACTION_UPDATE
+  if (status === ApplicationStatus.Draft) action = strings.ACTION_CONTINUE
 
   return (
     <>
