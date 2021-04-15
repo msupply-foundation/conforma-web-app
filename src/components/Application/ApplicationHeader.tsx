a58df2835b4fa9d78362962ebdd252315423a3e6
nmadruga <katinachovansky@gmail.com>
2021-04-16 11:30:29 +1200

    Improve label back to application list

--- src/components/Application/ApplicationHeader.tsx
+++ src/components/Application/ApplicationHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Container, Header, Icon, Segment } from 'semantic-ui-react'
+import { Container, Header, Icon, Label } from 'semantic-ui-react'
 import strings from '../../utils/constants'
 import { TemplateDetails, User } from '../../utils/types'
 
@@ -14,9 +14,9 @@ const ApplicationHeader: React.FC<AppHeaderProps> = ({ template, currentUser, Ch
   const { code, name } = template
   return (
     <Container id="application-area">
-      <Button
-        className="back"
+      <Label
         icon
+        size="medium"
         as={Link}
         to={`/applications?type=${code}`}
         content={
@@ -29,7 +29,7 @@ const ApplicationHeader: React.FC<AppHeaderProps> = ({ template, currentUser, Ch
       <Header as="h4" className="company-title" textAlign="center">
         {currentUser?.organisation?.orgName || strings.TITLE_NO_ORGANISATION}
       </Header>
-      <Container className="rectangle">
+      <Container id="application-content">
         <Header as="h2" textAlign="center">
           {`${name} ${strings.TITLE_APPLICATION_FORM}`}
           <Header.Subheader as="h3" content={strings.TITLE_INTRODUCTION} />
