342b2bd7e8bb698ff900a2ad1e7b854d061cfba7
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-05-05 17:36:26 +1200

    Summary page WIP

--- src/components/Sections/SectionWrapper.tsx
+++ src/components/Sections/SectionWrapper.tsx
@@ -36,22 +36,18 @@ const SectionWrapper: React.FC<SectionProps> = ({
   const stickyRef = useRef(null)
   return (
     <div ref={stickyRef} key={`${section.details.id}`}>
-      <Accordion style={sectionStyles.sup}>
+      <Accordion className="summary-section">
         <Accordion.Title active={isActive} onClick={toggleSection}>
           <Sticky context={stickyRef} offset={135} bottomOffset={150}>
-            <Grid columns="equal" style={sectionStyles.body}>
-              <Grid.Column floated="left">
-                <Header as="h2" content={details.title} style={sectionStyles.title} />
+            <Grid columns="equal" className="summary-section-header">
+              <Grid.Column>
+                <Header as="h4" content={details.title} />
               </Grid.Column>
-              <Grid.Column floated="right" textAlign="right">
+              <Grid.Column textAlign="right">
                 {extraSectionTitleContent && extraSectionTitleContent(section)}
               </Grid.Column>
-              <Grid.Column floated="right" textAlign="right" width={1}>
-                <Icon
-                  style={sectionStyles.icon}
-                  name={isActive ? 'angle up' : 'angle down'}
-                  size="large"
-                />
+              <Grid.Column textAlign="right" width={1}>
+                <Icon name={isActive ? 'angle up' : 'angle down'} size="large" />
               </Grid.Column>
             </Grid>
           </Sticky>
@@ -60,7 +56,9 @@ const SectionWrapper: React.FC<SectionProps> = ({
           {Object.values(pages).map((page) => (
             <div key={`${section.details.id}Page_${page.number}Container`}>
               {scrollableAttachment && scrollableAttachment(page)}
-              <p style={sectionStyles.page}>{page.name}</p>
+              <Header as="h6" className="summary-page-header">
+                {page.name}
+              </Header>
               <PageElements
                 key={`${section.details.id}Page_${page.number}`}
                 elements={page.state}
@@ -79,39 +77,6 @@ const SectionWrapper: React.FC<SectionProps> = ({
       </Accordion>
     </div>
   )
-}
-
-// Styles - TODO: Move to LESS || Global class style (semantic)
-const sectionStyles = {
-  sup: {
-    borderRadius: 8,
-    marginBottom: 10,
-    padding: 10,
-    border: 'none',
-    boxShadow: 'none',
-    backgroundColor: '#DCDDDD', // Invision
-  } as CSSProperties,
-  body: {
-    margin: 0,
-    borderRadius: 8,
-    backgroundColor: '#DCDDDD', // Invision
-  } as CSSProperties,
-  title: {
-    color: '#4A4A4A',
-    fontSize: 18,
-    fontWeight: 900,
-    textTransform: 'uppercase',
-    letterSpacing: 1,
-  } as CSSProperties,
-  icon: { color: 'rgb(100, 100, 100)' } as CSSProperties,
-  page: {
-    color: '#4A4A4A',
-    fontSize: 15,
-    fontWeight: 900,
-    textTransform: 'uppercase',
-    letterSpacing: 1,
-    marginTop: 8,
-  } as CSSProperties,
 }
 
 export default SectionWrapper
