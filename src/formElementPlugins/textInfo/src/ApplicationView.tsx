11bc070315cd37648316641faf5a92ea85459fee
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-05-06 23:38:32 +1200

    TextInfo styles

--- src/formElementPlugins/textInfo/src/ApplicationView.tsx
+++ src/formElementPlugins/textInfo/src/ApplicationView.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
-import { Message } from 'semantic-ui-react'
+import { Message, MessageContentProps } from 'semantic-ui-react'
 import { ApplicationViewProps } from '../../types'
 
 const ApplicationView: React.FC<ApplicationViewProps> = ({ parameters, Markdown }) => {
-  return (
-    <div className="text-info">
-      {/* <Message> */}
-      <Markdown text={parameters.title} />
-      <Markdown text={parameters.text} />
-      {/* </Message> */}
-    </div>
-  )
+  console.log('Parameters', parameters)
+  const { title, text, style } = parameters
+  if (['basic', 'info', 'warning', 'success', 'positive', 'negative', 'error'].includes(style)) {
+    const basic = style === 'basic'
+    const info = style === 'info'
+    const warning = style === 'warning'
+    const positive = style === 'success' || style === 'positive'
+    const negative = style === 'negative' || style === 'error'
+
+    return (
+      <div className="text-info">
+        <Message
+          basic={basic}
+          info={info}
+          warning={warning}
+          positive={positive}
+          negative={negative}
+          visible
+        >
+          <Markdown text={title} />
+          <Markdown text={text} />
+        </Message>
+      </div>
+    )
+  } else
+    return (
+      <div className="text-info">
+        <Markdown text={title} />
+        <Markdown text={text} />
+      </div>
+    )
 }
 
 export default ApplicationView
