cbdf369a550dd6fa2ebadcf585064f7d4cf47d70
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-05-11 22:22:48 +1200

    Remove unused Semantic imports

--- src/components/PageElements/PageElements.tsx
+++ src/components/PageElements/PageElements.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Grid, Segment } from 'semantic-ui-react'
+import { Form, Segment } from 'semantic-ui-react'
 import {
   ApplicationDetails,
   ElementState,
