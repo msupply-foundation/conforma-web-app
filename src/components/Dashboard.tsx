275040f18ccef37f150fb988502d35fb95aeff78
andreievg <andrei.evguenov@gmail.com>
2021-05-14 12:51:55 +1200

    remove unused

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Header, Button, Icon, SemanticCOLORS, SemanticICONS } from 'semantic-ui-react'
 import { useUserState } from '../contexts/UserState'
