5a609b9557f5ca736f5f4adedb588a3bfb605ebb
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-05-24 20:06:25 +1200

    WIP

--- utils/push_docs_to_wiki.sh
+++ utils/push_docs_to_wiki.sh
@@ -1,4 +1,4 @@
 python3 ./utils/convert_docs_to_wiki.py
-# git -C ./documentation/_wiki add -A
-# git -C ./documentation/_wiki commit -m "Update documentation"
-# git -C ./documentation/_wiki push
+git -C ./documentation/_wiki add -A
+git -C ./documentation/_wiki commit -m "Update documentation"
+git -C ./documentation/_wiki push
