afbf61894ecf993af575d2b77eb5d6db2f0299ed
nmadruga <nmadruga@users.noreply.github.com>
2021-05-26 16:13:02 +1200

    Improve generateReviewValidity after changes requested started to be checked also
    Co-authored-by: Andrei <andrei.evguenov@gmail.com>

--- src/utils/helpers/structure/generateReviewValidity.ts
+++ src/utils/helpers/structure/generateReviewValidity.ts
@@ -16,13 +16,30 @@ const generateReviewValidity = (newStructure: FullStructure) => {
 
   const incompletedUpdatesToChangesRequest = totalChangeRequests > doneChangeRequests
   const incompletedReviews = doneNonConform === 0 && totalReviewable > doneConform
+  const pageIsNotValid = ({ reviewProgress, changeRequestsProgress }: Page) => {
+    const { doneChangeRequests = 0, totalChangeRequests = 0 } = changeRequestsProgress || {}
+    // Not valid if remaining change requests
+    if (doneChangeRequests !== totalChangeRequests) return true
+    const { totalReviewable, doneConform } = reviewProgress || {}
+    // Valid if all conform
+    if (totalReviewable === doneConform) return false
+    // Valid if at least one non conform
+    if (doneNonConform > 0) return false
+    // Otherwise invalid
+    return true
+  }
 
   if (incompletedUpdatesToChangesRequest || incompletedReviews) {
-    const firstIncomplete = sortedPages.find(
-      ({ reviewProgress }) =>
-        reviewProgress?.totalReviewable !==
-        (reviewProgress?.doneConform || 0) + (reviewProgress?.doneNonConform || 0)
-    )
+    const firstIncomplete = sortedPages.find(pageIsNotValid)
+
+    if (!firstIncomplete) return
+    else
+      firstIncompleteReviewPage = {
+        sectionCode: firstIncomplete.sectionCode,
+        pageNumber: firstIncomplete.number,
+      }
+  }
+
 
     if (!firstIncomplete) return
     else
