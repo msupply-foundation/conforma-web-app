16d31ec2496764b7f0ba8e613f822b85ad092450
nmadruga <katinachovansky@gmail.com>
2021-05-28 21:58:16 +1200

    Show application title with link to summary on review page

--- src/components/Review/ReviewContainer.tsx
+++ src/components/Review/ReviewContainer.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import { Container, Header, Icon, Label } from 'semantic-ui-react'
+import { Container, Header, Icon, Label, Segment } from 'semantic-ui-react'
 import { useRouter } from '../../utils/hooks/useRouter'
 import { ApplicationDetails } from '../../utils/types'
 import strings from '../../utils/constants'
+import { Link } from 'react-router-dom'
 
 export interface ReviewContainerProps {
   application: ApplicationDetails
 }
 
 const ReviewContainer: React.FC<ReviewContainerProps> = ({ application, children }) => {
-  const { replace } = useRouter()
+  const {
+    query: { serialNumber },
+    push,
+  } = useRouter()
   const { template, name, org } = application
   return (
     <Container id="review-area">
       <div className="top-container">
         <Label
           className="back-label clickable"
-          onClick={() => replace(`/applications?type=${template.code}`)}
+          onClick={() => push(`/applications?type=${template.code}`)}
           content={
             <>
               <Icon name="chevron left" className="dark-grey" />
@@ -28,6 +32,14 @@ const ReviewContainer: React.FC<ReviewContainerProps> = ({ application, children
           {org?.name || strings.TITLE_NO_ORGANISATION}
         </Header>
       </div>
+      <Segment className="review-application-segment" basic textAlign="center">
+        <Header
+          as="h2"
+          className="clickable"
+          onClick={() => push(`/application/${serialNumber}`)}
+          content={name}
+        />
+      </Segment>
       {children}
     </Container>
   )
