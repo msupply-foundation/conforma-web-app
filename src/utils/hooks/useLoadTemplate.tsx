3fc6a300a60a9e78100b940ce1d24b8721c7e81d
andreievg <andrei.evguenov@gmail.com>
2021-06-08 15:24:10 +1200

    import templateElementCategory in useLoadTemplate

--- src/utils/hooks/useLoadTemplate.tsx
+++ src/utils/hooks/useLoadTemplate.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import {
   GetTemplateQuery,
   Template,
+  TemplateElementCategory,
   TemplateSection,
   useGetTemplateQuery,
 } from '../generated/graphql'
