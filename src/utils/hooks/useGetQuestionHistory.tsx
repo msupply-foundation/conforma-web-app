0f68068ca6d085857b38298989f7aac7c36c6174
nmadruga <katinachovansky@gmail.com>
2021-06-21 15:47:11 +1200

    New hook to create the list of history to display

---
+++ src/utils/hooks/useGetQuestionHistory.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react'
+import {
+  ReviewResponseDecision,
+  TemplateElement,
+  useGetHistoryForApplicantQuery,
+  useGetHistoryForReviewerQuery,
+  User,
+} from '../generated/graphql'
+import { HistoryElement } from '../types'
+import strings from '../../utils/constants'
+
+interface UseGetQuestionHistoryProps {
+  serial: string
+  templateCode: string
+  questionCode: string
+  userId: number
+  userLevel: number
+  isApplicant: boolean
+}
+
+interface ResponsesByDate {
+  [date: string]: HistoryElement
+}
+
+const useGetQuestionHistory = ({ isApplicant, ...variables }: UseGetQuestionHistoryProps) => {
+  const [historyList, setHistoryList] = useState<HistoryElement[]>([])
+  const { data, error, loading } = isApplicant
+    ? useGetHistoryForApplicantQuery({ variables })
+    : useGetHistoryForReviewerQuery({ variables })
+
+  useEffect(() => {
+    if (!data) return
+    // Organise ReviewResponse and Applicant Responses from latest to oldest
+    const { applicationResponses, reviewResponses } =
+      data?.templateElementByTemplateCodeAndCode as TemplateElement
+
+    const allResponses: ResponsesByDate = {}
+
+    applicationResponses.nodes.forEach((applicantResponse) => {
+      if (!applicantResponse) return
+      const { timeUpdated, application, value } = applicantResponse
+      const { firstName, lastName } = application?.user as User
+      // Set each entry using HistoryElement values
+      allResponses[timeUpdated] = {
+        author: firstName || '' + ' ' + lastName || '',
+        title: strings.TITLE_HISTORY_SUBMITTED_BY_APPLICANT,
+        message: value.text,
+        timeUpdated,
+      }
+    })
+
+    reviewResponses.nodes.forEach((reviewResponse) => {
+      if (!reviewResponse) return
+      const { timeUpdated, decision, comment, review } = reviewResponse
+      const { id, firstName, lastName } = review?.reviewer as User
+      // Set each entry using HistoryElement values
+      allResponses[timeUpdated] = {
+        author: firstName || '' + ' ' + lastName || '',
+        title: strings.TITLE_HISTORY_SUBMITTED_BY_APPLICANT,
+        message: decision as ReviewResponseDecision, // TODO: Localisation
+        timeUpdated,
+        reviewerComment: comment || '',
+      }
+      // TODO: Need to check for 2 responses in the same time?
+    })
+
+    setHistoryList(Object.values(allResponses))
+    // Find stages - Maybe done straight into HistoryPanel - or have in HistoryStructure?
+  }, [data])
+
+  return {
+    error,
+    loading,
+    historyList,
+  }
+}
+
+export default useGetQuestionHistory
