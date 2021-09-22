a5bd61dad171ec1808ffaba5b6b154f8082e01ff
nmadruga <katinachovansky@gmail.com>
2021-09-22 12:07:38 +1200

    Quick fix to allow viewing lookup tables inserted with plural names

--- src/LookupTable/graphql/getDynamicSingleTable.query.ts
+++ src/LookupTable/graphql/getDynamicSingleTable.query.ts
@@ -3,7 +3,7 @@ import { buildFieldList, capitalizeFirstLetter, toCamelCase } from '../utils'
 
 const getDynamicSingleTable = (structure: any) => gql`
     query getDynamicSingleTable {
-      lookupTable${capitalizeFirstLetter(toCamelCase(structure.name))}s {
+      lookupTable${capitalizeFirstLetter(toCamelCase(structure.name))} {
         nodes {
           ${buildFieldList(structure.fieldMap)}
         }
