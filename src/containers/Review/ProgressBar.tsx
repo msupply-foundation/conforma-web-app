358c419029e5fa560739a2c447a4179d99a9654d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-01-27 00:17:49 +1300

    Progress with most styling

--- src/containers/Review/ProgressBar.tsx
+++ src/containers/Review/ProgressBar.tsx
@@ -1,22 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Message } from 'semantic-ui-react'
-import { useUserState } from '../../contexts/UserState'
+import React from 'react'
+import { Icon, SemanticICONS } from 'semantic-ui-react'
 import { FullStructure } from '../../utils/types'
-import { useLanguageProvider } from '../../contexts/Localisation'
+// @ts-ignore -- no types provided
+import { Stepper, Step, StepTitle } from 'react-progress-stepper'
+import { ApplicationOutcome } from '../../utils/generated/graphql'
 
 const ReviewProgress: React.FC<{
   structure: FullStructure
 }> = ({ structure: fullStructure }) => {
-  const { strings } = useLanguageProvider()
   const {
-    userState: { currentUser },
-  } = useUserState()
+    stages,
+    info: {
+      outcome,
+      current: { stage: currentStage },
+    },
+  } = fullStructure
+
+  const [currentStageClass, currentStageIcon]: [string, SemanticICONS] =
+    outcome === ApplicationOutcome.Rejected ? ['rejected', 'remove'] : ['', 'circle']
+
+  const steps = [
+    <Step key="first">
+      <StepTitle>Submitted</StepTitle>
+    </Step>,
+    ...stages.map((stage) =>
+      stage.number === currentStage.number ? (
+        <Step
+          key={stage.name}
+          customContent={() => (
+            <div className={`step-custom-content ${currentStageClass}`}>
+              <Icon name={currentStageIcon} />
+            </div>
+          )}
+        >
+          <StepTitle>{stage.name}</StepTitle>
+        </Step>
+      ) : (
+        <Step key={stage.name}>
+          <StepTitle>{stage.name}</StepTitle>
+        </Step>
+      )
+    ),
+  ]
 
   return (
     <div id="review-progress">
-      <Message size="huge">
-        <Message.Header>Placeholder for PROGRESS BAR</Message.Header>
-      </Message>
+      <div id="stepper-wrapper">
+        <Stepper id="stepper" step={currentStage.number} numbered={false}>
+          {steps}
+        </Stepper>
+      </div>
     </div>
   )
 }
