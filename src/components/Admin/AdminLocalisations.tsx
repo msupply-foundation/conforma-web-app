6c56dc9633675df10bd09d7d466a14959449a09d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-02-10 22:14:58 +1300

    UI for language enable/disable

--- src/components/Admin/AdminLocalisations.tsx
+++ src/components/Admin/AdminLocalisations.tsx
@@ -18,12 +18,33 @@ export const AdminLocalisations: React.FC = () => {
     )
   }, [])
 
+  useEffect(() => {
+    if (installedLanguages.length === 0) return
+    // To-do: update on server
+    console.log('TO-DO: Updating...')
+  }, [installedLanguages])
+
+  const handleSelect = (language: LanguageOption, index: number) => {
+    const enabled = language.enabled
+    const newLanguages = [...installedLanguages]
+    newLanguages[index] = { ...language, enabled: !enabled }
+    setInstalledLanguages(newLanguages)
+  }
+
   return (
     <div id="localisation-panel">
       <Header as="h1">Localisation</Header>
       <Header as="h4">Currently installed languages</Header>
-      {installedLanguages.map((language) => (
-        <LanguageRow language={language} />
+      <div className="flex-row">
+        <p className="smaller-text">Enabled</p>
+      </div>
+      {installedLanguages.map((language, index) => (
+        <LanguageRow
+          language={language}
+          key={language.code}
+          index={index}
+          handleSelect={handleSelect}
+        />
       ))}
       <Header as="h3">Export language files as spreadsheet:</Header>
       <div className="flex-row-center-center" style={{ gap: 20 }}>
@@ -40,6 +61,31 @@ export const AdminLocalisations: React.FC = () => {
   )
 }
 
-const LanguageRow: React.FC<{ language: LanguageOption }> = ({ language }) => {
-  return <div>{language.languageName}</div>
+const LanguageRow: React.FC<{
+  language: LanguageOption
+  handleSelect: (language: LanguageOption, index: number) => void
+  index: number
+}> = ({ language, handleSelect, index }) => {
+  return (
+    <div
+      className="flex-row-start-center"
+      style={{
+        gap: 20,
+        maxWidth: '80%',
+        backgroundColor: 'white',
+        marginBottom: 5,
+        padding: 10,
+        borderRadius: 8,
+      }}
+    >
+      <Checkbox checked={language.enabled} onChange={() => handleSelect(language, index)} />
+      <div className="flex-column">
+        <p style={{ marginBottom: 0 }}>
+          <strong>{language.languageName}</strong>
+        </p>
+        <p className="smaller-text">{language.description}</p>
+      </div>
+      <p style={{ fontSize: '2em', flexGrow: 1, textAlign: 'right' }}>{language.flag}</p>
+    </div>
+  )
 }
