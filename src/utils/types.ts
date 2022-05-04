0d703ed36548d85d1389fd6fc72bb2cbcf15bba1
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-05-04 17:03:40 +1200

    Reformat

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -486,7 +486,6 @@ export interface AssignedSectionsByLevel {
   [level: string]: SectionAssignee
 }
 
-
 interface SectionAssignee {
   [sectionCode: string]: {
     newAssignee: number | undefined
@@ -757,15 +756,15 @@ export type FilterTypeMethod = (filterKey: string, options?: FilterTypeOptions) 
 
 export type FilterTypeDefinitions = {
   [filterType in
-  | 'number'
-  | 'date'
-  | 'boolean'
-  | 'equals'
-  | 'enumList'
-  | 'searchableListIn'
-  | 'searchableListInArray'
-  | 'staticList'
-  | 'search']: FilterTypeMethod
+    | 'number'
+    | 'date'
+    | 'boolean'
+    | 'equals'
+    | 'enumList'
+    | 'searchableListIn'
+    | 'searchableListInArray'
+    | 'staticList'
+    | 'search']: FilterTypeMethod
 }
 
 export type FilterTypes = keyof FilterTypeDefinitions
