ab36e849e3b5ee71f6c2d7593015f0e73fc49b87
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-05-25 17:07:08 +1200

    Put export in try/catch block so we can notify on failure

--- src/utils/localisation/exportLanguages.ts
+++ src/utils/localisation/exportLanguages.ts
@@ -13,76 +13,80 @@ export interface LanguageObject extends LanguageOption {
 export const exportLanguages = async (
   languageOptions: LanguageOption[]
 ): Promise<{ success: boolean; message: string }> => {
-  // Fetch all languages
-  const allLanguageStrings: { [key: string]: LanguageStrings } = await getRequest(
-    `${config.serverREST}/admin/all-languages`
-  )
+  try {
+    // Fetch all languages
+    const allLanguageStrings: { [key: string]: LanguageStrings } = await getRequest(
+      `${config.serverREST}/admin/all-languages`
+    )
 
-  // Combine language options with their translations
-  const languageObject: LanguageObject[] = languageOptions.map((language) => ({
-    ...language,
-    translations: allLanguageStrings[language.code],
-  }))
+    // Combine language options with their translations
+    const languageObject: LanguageObject[] = languageOptions.map((language) => ({
+      ...language,
+      translations: allLanguageStrings[language.code],
+    }))
 
-  // Iterate over default strings and extract all translations
-  const translationsObject = mapValues(defaultLanguageStrings, (value, key) =>
-    getTranslations(value, key as keyof LanguageStrings, languageObject)
-  )
+    // Iterate over default strings and extract all translations
+    const translationsObject = mapValues(defaultLanguageStrings, (value, key) =>
+      getTranslations(value, key as keyof LanguageStrings, languageObject)
+    )
 
-  // Assemble orphans
-  const orphanKeys = languageObject.map((lang) => Object.keys(lang.translations)).flat()
-  const orphansObject = Object.fromEntries(
-    orphanKeys.map((key) => [
+    // Assemble orphans
+    const orphanKeys = languageObject.map((lang) => Object.keys(lang.translations)).flat()
+    const orphansObject = Object.fromEntries(
+      orphanKeys.map((key) => [
+        key,
+        getTranslations('', key as keyof LanguageStrings, languageObject),
+      ])
+    )
+
+    // Build CSV Rows and Columns
+    const row1Name = [
+      'Language:',
+      'System Default',
+      ...languageOptions.map((opt) => opt.languageName),
+    ]
+    const row2Description = [
+      'Description:',
+      'Internal values from which all others are translated',
+      ...languageOptions.map((opt) => opt.description),
+    ]
+    const row3code = ['Code:', '', ...languageOptions.map((opt) => opt.code)]
+    const row4Flag = ['Flag:', '', ...languageOptions.map((opt) => opt.flag)]
+    const row5Enabled = ['Enabled?', '', ...languageOptions.map((opt) => opt.enabled)]
+    const translationRows = Object.keys(defaultLanguageStrings).map((key) => [
       key,
-      getTranslations('', key as keyof LanguageStrings, languageObject),
+      ...Object.values(translationsObject[key as keyof LanguageStrings]),
     ])
-  )
+    const orphanRows = orphanKeys.map((key) => [
+      key,
+      ...Object.values(orphansObject[key as keyof LanguageStrings]),
+    ])
 
-  // Build CSV Rows and Columns
-  const row1Name = [
-    'Language:',
-    'System Default',
-    ...languageOptions.map((opt) => opt.languageName),
-  ]
-  const row2Description = [
-    'Description:',
-    'Internal values from which all others are translated',
-    ...languageOptions.map((opt) => opt.description),
-  ]
-  const row3code = ['Code:', '', ...languageOptions.map((opt) => opt.code)]
-  const row4Flag = ['Flag:', '', ...languageOptions.map((opt) => opt.flag)]
-  const row5Enabled = ['Enabled?', '', ...languageOptions.map((opt) => opt.enabled)]
-  const translationRows = Object.keys(defaultLanguageStrings).map((key) => [
-    key,
-    ...Object.values(translationsObject[key as keyof LanguageStrings]),
-  ])
-  const orphanRows = orphanKeys.map((key) => [
-    key,
-    ...Object.values(orphansObject[key as keyof LanguageStrings]),
-  ])
+    const csvRows = [
+      row1Name,
+      row2Description,
+      row3code,
+      row4Flag,
+      row5Enabled,
+      [],
+      ['KEYS'],
+      ...translationRows,
+      [],
+      ['__ORPHANS__', 'Translations found in language files that are no longer in use'],
+      ...orphanRows,
+    ]
+    const csvContent = 'data:text/csv;charset=utf-8,' + Papa.unparse(csvRows)
+    const encodedUri = encodeURI(csvContent)
+    const hiddenLink = document.createElement('a')
+    hiddenLink.setAttribute('href', encodedUri)
+    hiddenLink.setAttribute('download', `conforma_localisations_${DateTime.now().toISODate()}.csv`)
+    document.body.appendChild(hiddenLink) // Required for FF
+    hiddenLink.click() // This will download the data file.
 
-  const csvRows = [
-    row1Name,
-    row2Description,
-    row3code,
-    row4Flag,
-    row5Enabled,
-    [],
-    ['KEYS'],
-    ...translationRows,
-    [],
-    ['__ORPHANS__', 'Translations found in language files that are no longer in use'],
-    ...orphanRows,
-  ]
-  const csvContent = 'data:text/csv;charset=utf-8,' + Papa.unparse(csvRows)
-  const encodedUri = encodeURI(csvContent)
-  const hiddenLink = document.createElement('a')
-  hiddenLink.setAttribute('href', encodedUri)
-  hiddenLink.setAttribute('download', `conforma_localisations_${DateTime.now().toISODate()}.csv`)
-  document.body.appendChild(hiddenLink) // Required for FF
-  hiddenLink.click() // This will download the data file.
-
-  return { success: true, message: '' }
+    return { success: true, message: '' }
+  } catch (err) {
+    return { success: false, message: err.message }
+  }
 }
 
 const getTranslations = (
