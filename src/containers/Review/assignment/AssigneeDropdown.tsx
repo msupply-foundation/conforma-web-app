e69f6f0746223c0aaa4e0df5515c7ba691eff22c
Nicole Madruga <katinachovansly@gmail.com>
2022-11-04 13:45:04 +1300

    Dropdown scrollable and with not_assigned showing on top

--- src/containers/Review/assignment/AssigneeDropdown.tsx
+++ src/containers/Review/assignment/AssigneeDropdown.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
+import { useLanguageProvider } from '../../../contexts/Localisation'
 import { AssignmentOptions } from '../../../utils/types'
 interface AssigneeProps {
   assignmentOptions: AssignmentOptions
@@ -8,16 +9,29 @@ interface AssigneeProps {
 }
 
 const AssigneeDropdown: React.FC<AssigneeProps> = ({ assignmentOptions, onChangeMethod }) => {
+  const { strings } = useLanguageProvider()
   const { isCompleted, options, selected } = assignmentOptions
 
   return (
     <Dropdown
       className="reviewer-dropdown"
-      options={options}
       value={selected}
       disabled={isCompleted}
-      onChange={(_: any, { value }: any) => onChangeMethod(value as number)}
-    />
+      placeholder={strings.ASSIGNMENT_NOT_ASSIGNED}
+      labeled
+      scrolling
+    >
+      <Dropdown.Menu>
+        <Dropdown.Header icon="tags" content={strings.ASSIGNMENT_NOT_ASSIGNED} />
+        <Dropdown.Divider />
+        {options.map((assignee) => (
+          <Dropdown.Item
+            {...assignee}
+            onClick={(_: any, { value }: any) => onChangeMethod(value as number)}
+          />
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
 
