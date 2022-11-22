5ce6b5d271ede912f593b3da1c7e58303ccb248d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-11-22 15:32:41 +1300

    Improvements

--- src/formElementPlugins/imageDisplay/src/SummaryView.tsx
+++ src/formElementPlugins/imageDisplay/src/SummaryView.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
+import { allowedSizeValues } from './ApplicationView'
 import { SummaryViewProps } from '../../types'
 
 const SummaryView: React.FC<SummaryViewProps> = ({ parameters }) => {
-  const { url, size, alignment, altText } = parameters
+  const { url, size, alignment, altText, summarySize = 'small' } = parameters
+
+  // By default, image is displayed "small" in Summary view, but can be
+  // over-ridden by specifying "summarySize"
 
   return (
     <Image
       src={url}
-      size="small" // Always show small in Summary view
+      size={allowedSizeValues.includes(summarySize) ? size : 'small'}
       // Have to do a bit of trickery in terms margin over-rides to get
       // right-alignment working
       centered={alignment === 'center' || alignment === 'right'}
-      style={alignment === 'right' ? { marginRight: 'unset' } : {}}
+      style={alignment === 'right' ? { marginRight: 'unset' } : { maxWidth: 100 }}
       alt={altText}
       title={altText}
     />
