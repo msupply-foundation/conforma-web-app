ddf872eebcc2256cf005f1f80e4d8dcf238dc138
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-05-02 10:37:13 +1200

    Better implementation?

--- src/formElementPlugins/useDefault.ts
+++ src/formElementPlugins/useDefault.ts
@@ -20,33 +20,33 @@ const useDefault = ({
   additionalDependencies = [],
 }: UseDefaultProps) => {
   const [isInitialLoad, setIsInitialLoad] = useState(true)
+  const [prevDefaultValue, setPrevDefaultValue] = useState(defaultValue)
 
   const { persistUserInput = false, ignoreNullDefault = false } = parameters
 
   useEffect(() => {
-    let initial = isInitialLoad
-    if (parameters.label === 'Total price (FJ$)') {
-      console.log('defaultValue', defaultValue, initial)
-      console.log('parameters', parameters)
-    }
     if (defaultValue === loadingValue || defaultValue === undefined) return
     if (ignoreNullDefault && defaultValue === null) return
-
-    // This allows elements that have a hard-coded default (i.e. it'll never
-    // start 'undefined') to load without having to wait for subsequent default
-    // change (which won't happen)
-    if (defaultValue !== undefined && initial) initial = false
 
     // This prevents an existing response from being wiped out by a default on
     // first load, but allows it to still be replaced on subsequent default
     // changes
-    setIsInitialLoad(false)
-
-    if (!persistUserInput) return
-
-    if (!currentResponse?.text && !isInitialLoad && !initial) {
-      onChange(defaultValue)
+    if (isInitialLoad) {
+      if (!currentResponse?.text) {
+        onChange(defaultValue)
+      }
+      setIsInitialLoad(false)
+      return
     }
+
+    if (!currentResponse?.text) {
+      onChange(defaultValue)
+      return
+    }
+
+    if (persistUserInput) return
+
+    onChange(defaultValue)
   }, [defaultValue, ...additionalDependencies])
 }
 
