daea8c92f2e4957f17ec6e0a9f5cc42a63965041
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-06-29 17:05:12 +1200

    Implement new plugin strings model

--- src/contexts/Localisation.tsx
+++ src/contexts/Localisation.tsx
@@ -1,9 +1,10 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
 import config from '../config'
-import strings from '../utils/defaultLanguageStrings'
+import defaultStrings from '../utils/defaultLanguageStrings'
 import { getRequest } from '../utils/helpers/fetchMethods'
+import { pluginProvider } from '../formElementPlugins'
 import getServerUrl from '../utils/helpers/endpoints/endpointUrlBuilder'
-import { mapValues } from 'lodash'
+import { mapValues, mapKeys } from 'lodash'
 import Markdown from '../utils/helpers/semanticReactMarkdown'
 
 const { pluginsFolder } = config
@@ -38,7 +39,7 @@ export type LanguageOption = {
   enabled: boolean
 }
 
-export type LanguageStrings = { [Property in keyof typeof strings]: string }
+export type LanguageStrings = { [Property in keyof typeof defaultStrings]: string }
 
 interface LanguageState {
   languageOptions: LanguageOption[]
@@ -50,10 +51,13 @@ interface LanguageState {
 
 type Substitutions = Record<string, unknown> | string | number
 
-export type TranslateMethod = (key: keyof typeof strings, substitutions?: Substitutions) => string
+export type TranslateMethod = (
+  key: keyof typeof defaultStrings,
+  substitutions?: Substitutions
+) => string
 
 type TranslateMarkdownMethod = (
-  key: keyof typeof strings,
+  key: keyof typeof defaultStrings,
   substitutions?: Substitutions
 ) => JSX.Element
 
@@ -89,6 +93,20 @@ const initialContext: {
 
 const LanguageProviderContext = createContext(initialContext)
 
+// Get localisation strings from all form element plugins
+export const getPluginStrings = () => {
+  let pluginStrings = {}
+  for (const plugin of Object.values(pluginProvider.pluginManifest)) {
+    try {
+      const strings = require(`../${config.pluginsFolder}/${plugin.folderName}/localisation.json`)
+      pluginStrings = { ...pluginStrings, ...mapKeys(strings, (_, key) => `${plugin.code}.${key}`) }
+    } catch {}
+  }
+  return pluginStrings
+}
+
+const allDefaultStrings = { ...defaultStrings, ...getPluginStrings() }
+
 export function LanguageProvider({
   children,
   languageOptions,
@@ -98,7 +116,7 @@ export function LanguageProvider({
   const [languageState, setLanguageState] = useState<LanguageState>({
     languageOptions,
     selectedLanguage: initSelectedLanguage,
-    strings,
+    strings: allDefaultStrings,
     loading: true,
     error: null,
   })
@@ -106,19 +124,6 @@ export function LanguageProvider({
     savedLanguageCode ?? defaultLanguageCode
   )
   const [shouldRefetchStrings, setShouldRefetchStrings] = useState(false)
-
-  // Helper function provided to plugins to determine where to read their
-  // language strings from
-  const getPluginStrings = (plugin: string) => {
-    const defaultPluginStrings = require(`../${pluginsFolder}/${plugin}/localisation.json`)
-    if (selectedLanguageCode === 'default') return defaultPluginStrings
-    try {
-      const localisedPluginStrings = require(`../${pluginsFolder}/${plugin}/localisation/${selectedLanguageCode}/strings.json`)
-      return consolidateStrings(defaultPluginStrings, localisedPluginStrings)
-    } catch {
-      return defaultPluginStrings
-    }
-  }
 
   // Load initial language and fetch options list
   const updateLanguageState = async (languageCode: string) => {
@@ -183,9 +188,12 @@ export function LanguageProvider({
     )
   }
 
+  // Same as the regular "translate" function, except appends the pluginCode as
+  // key. We use the pluginCode as part of the key to avoid collisions if
+  // plugins happen to use the same keys as the main app.
   const translatePlugin = (pluginCode: string, key: string, substitutions: Substitutions = {}) => {
-    const pluginStrings = getPluginStrings(pluginCode)
-    return getTranslation(pluginStrings, key, substitutions)
+    const fullKey = `${pluginCode}.${key}`
+    return getTranslation(languageState.strings, fullKey, substitutions)
   }
 
   // Fetch new language when language code changes
@@ -231,12 +239,12 @@ export const useLanguageProvider = () => useContext(LanguageProviderContext)
 
 const getLanguageStrings = async (code: string) => {
   // If default language code not available on server, just use local defaults
-  if (code === 'default') return strings
+  if (code === 'default') return allDefaultStrings
   // Else fetch language file from server
   try {
     const fetchedStrings = await getRequest(getServerUrl('language', { code }))
     if (fetchedStrings?.error) throw new Error(`Language code: ${code}, ${fetchedStrings?.message}`)
-    return consolidateStrings(strings, fetchedStrings)
+    return consolidateStrings(allDefaultStrings, fetchedStrings)
   } catch (err) {
     throw err
   }
