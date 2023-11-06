115bf70ecd7dc7dab316f9be65cd855a52d05e94
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-11-06 20:23:34 +1300

    Show Help docs

--- src/contexts/SystemPrefs.tsx
+++ src/contexts/SystemPrefs.tsx
@@ -14,6 +14,7 @@ interface PrefsState {
     googleAnalyticsId?: string
     siteHost?: string
     style?: { headerBgColor?: string }
+    helpLinks?: { text: string; link: string }[]
   }
   languageOptions?: LanguageOption[]
   latestSnapshot?: string
