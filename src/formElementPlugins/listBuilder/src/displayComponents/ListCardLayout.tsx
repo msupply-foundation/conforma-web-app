e1be8ea2942a001a9afd8f7273a99df76d3cb173
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-03-18 23:46:12 +1300

    dataFormat and textFormat props

--- src/formElementPlugins/listBuilder/src/displayComponents/ListCardLayout.tsx
+++ src/formElementPlugins/listBuilder/src/displayComponents/ListCardLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, Icon, Label } from 'semantic-ui-react'
+import { substituteValues } from '../../../../utils/helpers/utilityFunctions'
 import { ListLayoutProps } from '../types'
-import { substituteValues } from '../helpers'
 
 const ListCardLayout: React.FC<ListLayoutProps> = ({
   listItems,
