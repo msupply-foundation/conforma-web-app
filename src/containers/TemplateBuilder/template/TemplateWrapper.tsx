3ab9697203bda4b76da81b775ab9bd02a6426028
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-10-09 10:11:15 +1300

    Add to UI

--- src/containers/TemplateBuilder/template/TemplateWrapper.tsx
+++ src/containers/TemplateBuilder/template/TemplateWrapper.tsx
@@ -142,6 +142,7 @@ export interface TemplateState {
   canApplicantMakeChanges: boolean
   dashboardRestrictions: string[] | null
   priority: number | null
+  staleDraftRetentionDays: number
   canEdit: boolean
 }
 
@@ -179,6 +180,7 @@ const defaultTemplateContextState: TemplateContextState = {
     canEdit: true,
     dashboardRestrictions: null,
     priority: null,
+    staleDraftRetentionDays: 90,
   },
   refetch: () => {},
   sections: [],
@@ -219,7 +221,7 @@ const TemplateWrapper: React.FC = () => {
           versionTimestamp: DateTime.fromISO(template?.versionTimestamp) || DateTime.now(),
           parentVersionId: template?.parentVersionId || null,
           versionHistory:
-            [...template?.versionHistory]
+            [...(template?.versionHistory ?? [])]
               .map((version, index) => ({
                 ...version,
                 number: index + 1,
@@ -238,6 +240,7 @@ const TemplateWrapper: React.FC = () => {
           canEdit: isTemplateUnlocked(template) && template.status === TemplateStatus.Draft,
           dashboardRestrictions: template?.dashboardRestrictions as string[] | null,
           priority: template.priority ?? null,
+          staleDraftRetentionDays: template.staleDraftRetentionDays,
         },
         category: (template?.templateCategory as TemplateCategory) || undefined,
         fromQuery: template,
